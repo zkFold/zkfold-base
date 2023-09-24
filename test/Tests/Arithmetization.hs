336de2845915c71b40ac8ed71f6eca28f2dcd9b0
Vladimir Sinyakov <vlasin@zkfold.io>
2023-09-24 20:20:54 +0200

    Update test

--- test/Tests/Arithmetization.hs
+++ test/Tests/Arithmetization.hs
@@ -3,6 +3,7 @@
 
 module Tests.Arithmetization (testArithmetization) where
 
+import           Data.Bifunctor                              (bimap)
 import           Data.List                                   (find)
 import           Prelude                                     hiding (not, Num(..), Eq(..), (^), (/))
 
@@ -20,30 +21,29 @@ instance Finite SmallField where
 instance Prime SmallField
 
 -- f x = if (2 / x == 3) then (x ^ 2 + 3 * x + 5) else (4 * x ^ 3)
-testFunc :: forall a b . (FromConstant Integer a, FiniteField a, GeneralizedEq b a, GeneralizedConditional b a) => a -> a
-testFunc x =
+testFunc :: forall a b . (FromConstant Integer a, FiniteField a, GeneralizedEq b a, GeneralizedConditional b a) => a -> a -> a
+testFunc x y =
     let c  = fromConstant @Integer @a
         g1 = x ^ (2 :: Integer) + c 3 * x + c 5
         g2 = c 4 * x ^ (3 :: Integer)
         g3 = c 2 / x
-    in (g3 == c 3 :: b) ? g1 $ g2 
+    in (g3 == y :: b) ? g1 $ g2
 
-testResult :: Zp SmallField -> Bool
-testResult x =
+testResult :: Zp SmallField -> Zp SmallField -> Bool
+testResult x y =
     let r = compile (testFunc @(R1CS (Zp SmallField)) @(SymbolicBool (R1CS (Zp SmallField))))
-        v = r1csValue $ apply @(R1CS (Zp SmallField)) @(R1CS (Zp SmallField)) r x
-    in head v == testFunc @(Zp SmallField) @Bool x
+        v = r1csValue $ foldl (apply @(R1CS (Zp SmallField)) @(R1CS (Zp SmallField))) r [x, y]
+    in head v == testFunc @(Zp SmallField) @Bool x y
 
 testArithmetization :: IO ()
 testArithmetization = do
-    let m   = [0..order @SmallField - 1]
-        res = zip m $ map (testResult . toZp) m
+    let m   = zipWith (curry (bimap toZp toZp)) [0..order @SmallField - 1] [0..order @SmallField - 1]
+        res = zip m $ map (uncurry testResult) m
     case find (not . snd) res of
         Nothing     -> print @String "Success!"
-        Just (i, _) -> do
+        Just (p@(x, y), _) -> do
             let r = compile (testFunc @(R1CS (Zp SmallField)) @(SymbolicBool (R1CS (Zp SmallField))))
-                x = toZp i
-            print @String $ "Failure at " ++ show i ++ "!"
+            print @String $ "Failure at " ++ show p ++ "!"
 
-            r1csPrint $ apply @(R1CS (Zp SmallField)) @(R1CS (Zp SmallField)) r x
-            print $ testFunc @(Zp SmallField) @Bool x
+            r1csPrint $ foldl (apply @(R1CS (Zp SmallField)) @(R1CS (Zp SmallField))) r [x, y]
+            print $ testFunc @(Zp SmallField) @Bool x y
