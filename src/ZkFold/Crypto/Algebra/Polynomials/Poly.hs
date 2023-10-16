ee854af55b396ebab32bcd0573fae060f2668579
vovunku <vovunku@mail.ru>
2023-10-16 22:08:09 +0200

    add show

--- src/ZkFold/Crypto/Algebra/Polynomials/Poly.hs
+++ src/ZkFold/Crypto/Algebra/Polynomials/Poly.hs
@@ -1,8 +1,13 @@
 module ZkFold.Crypto.Algebra.Polynomials.Poly where
 
+import           Prelude                           hiding (Num(..), (/), (!!), lcm, length, sum, take, drop)
+import           Data.List                         (intercalate)
 import           ZkFold.Crypto.Algebra.Basic.Class
 
 newtype Poly c = P [c]
+
+instance (Show c, Eq c, FiniteField c) => Show (Poly c) where
+    show (P cs) = intercalate " + " (zipWith (\ c i -> (if c == zero then "" else (if c == one then "" else show c) ++ "x^" ++ show i)) cs [1..])
 
 addPoly :: (FiniteField c) => Poly c -> Poly c -> Poly c
 addPoly (P l) (P r) = P (go l r)
--- test/Tests/Poly.hs
+++ test/Tests/Poly.hs
@@ -1,6 +1,21 @@
+{-# LANGUAGE TypeApplications    #-}
+
 module Tests.Poly (testPoly) where
+
 import           Prelude                                    hiding (not, Num(..), Eq(..), (^), (/))
+
+import           ZkFold.Crypto.Algebra.Basic.Class
+import           ZkFold.Crypto.Algebra.Polynomials.Poly
+
+import           Tests.Utility.Types                        (SmallField)
+import           ZkFold.Crypto.Algebra.Basic.Field (Zp)
+
+samplePoly :: Prime p => Poly (Zp p)
+samplePoly = P [1, 2 ,3]
 
 testPoly :: IO ()
 testPoly = do
     putStrLn "\nStarting Poly test...\n"
+
+    putStrLn "Test Poly: "
+    print $ samplePoly @SmallField
