61b467c01b03a828d2a42d2105c6fa8db5086fec
Vladimir Sinyakov <vlasin@zkfold.io>
2023-10-30 00:13:02 +0100

    Fix exports in ZkFold.Symbolic.Types

--- src/ZkFold/Symbolic/Types.hs
+++ src/ZkFold/Symbolic/Types.hs
@@ -1,4 +1,4 @@
-module ZkFold.Symbolic.Types (Symbolic, SmallField, R, I) where
+module ZkFold.Symbolic.Types (Symbolic, BLS12_381_Scalar, SmallField, R, I) where
 
 import           Prelude                          (Integer)
 
@@ -12,6 +12,11 @@ import           ZkFold.Symbolic.Data.Ord         (Ord)
 
 type Symbolic a = (FromConstant I a, FiniteField a, ToBits a, Eq (Bool a) a, Ord (Bool a) a, Conditional (Bool a) a)
 
+data BLS12_381_Scalar
+instance Finite BLS12_381_Scalar where
+    order = 52435875175126190479447740508185965837690552500527637822603658699938581184513
+instance Prime BLS12_381_Scalar
+
 data SmallField
 instance Finite SmallField where
     order = 97
@@ -19,8 +24,3 @@ instance Prime SmallField
 
 type R = ArithmeticCircuit (Zp SmallField)
 type I = Integer
-
-data BLS12_381_Scalar
-instance Finite BLS12_381_Scalar where
-    order = 52435875175126190479447740508185965837690552500527637822603658699938581184513
-instance Prime BLS12_381_Scalar
