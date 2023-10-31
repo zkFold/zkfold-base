88b434c03e5302a967242d07d57fee8ac4936590
Vladimir Sinyakov <vlasin@zkfold.io>
2023-10-31 10:03:51 +0100

    Update LEQ.hs
    Do not save the script

--- test/Examples/LEQ.hs
+++ test/Examples/LEQ.hs
@@ -5,7 +5,6 @@ module Examples.LEQ (exampleLEQ) where
 import           Prelude                          hiding (Num(..), Eq(..), Ord(..), Bool, (^), (/), (!!), (||), not, any)
 
 import           ZkFold.Base.Algebra.Basic.Field  (Zp)
-import           ZkFold.Prelude                   (writeFileJSON)
 import           ZkFold.Symbolic.Arithmetization  (acSizeM, acSizeN, ArithmeticCircuit)
 import           ZkFold.Symbolic.Data.Bool        (Bool(..))
 import           ZkFold.Symbolic.Data.Ord         (Ord(..))
@@ -19,11 +18,8 @@ leq x y = x <= y
 exampleLEQ :: IO ()
 exampleLEQ = do
     let ac   = compile @(Zp BLS12_381_Scalar) (leq @(ArithmeticCircuit (Zp BLS12_381_Scalar))) :: ArithmeticCircuit (Zp BLS12_381_Scalar)
-        file = "compiled_scripts/leq.json"
 
     putStrLn "\nExample: (<=) operation\n"
 
     putStrLn $ "Number of constraints: " ++ show (acSizeN ac)
     putStrLn $ "Number of variables: "   ++ show (acSizeM ac)
-    writeFileJSON file ac
-    putStrLn $ "Script saved: " ++ file
