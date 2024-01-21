3187912cbee49915b8305d6922f481f32e1a512a
Vladimir Sinyakov <vlasin@zkfold.io>
2024-01-21 01:41:43 -0300

    Remove Vesting Script example
    As we made changes to the  script signature, it does not make sense to keep this older example.

--- examples/Main.hs
+++ examples/Main.hs
@@ -1,18 +1,21 @@
 module Main where
 
 import           Prelude
-import           System.Directory            (createDirectoryIfMissing)
+import           System.Directory       (createDirectoryIfMissing)
 
-import           Examples.Eq                 (exampleEq)
-import           Examples.Fibonacci          (exampleFibonacci)
-import           Examples.LEQ                (exampleLEQ)
-import           Examples.MiMCHash           (exampleMiMC)
+import           Examples.Eq            (exampleEq)
+import           Examples.Fibonacci     (exampleFibonacci)
+import           Examples.LEQ           (exampleLEQ)
+import           Examples.MiMCHash      (exampleMiMC)
+import           Examples.ReverseList   (exampleReverseList)
 
 main :: IO ()
 main = do
     createDirectoryIfMissing True "compiled_scripts"
     
+    exampleReverseList
     exampleEq
     exampleFibonacci
     exampleMiMC
     exampleLEQ
+    
