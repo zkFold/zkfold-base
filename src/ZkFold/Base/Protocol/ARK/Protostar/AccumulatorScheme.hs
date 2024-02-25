facdd72b2eadb73b1361a6807dccff2c9dce6005
Vladimir Sinyakov <vlasin@zkfold.io>
2024-02-25 16:21:44 -0600

    Add accumulator scheme draft

---
+++ src/ZkFold/Base/Protocol/ARK/Protostar/AccumulatorScheme.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeOperators       #-}
+
+module ZkFold.Base.Protocol.ARK.Protostar.AccumulatorScheme where
+
+import           Prelude                         hiding (length)
+
+import ZkFold.Base.Protocol.ARK.Protostar.Accumulator
+import ZkFold.Base.Protocol.ARK.Protostar.FiatShamir (FiatShamir (..))
+import ZkFold.Base.Protocol.ARK.Protostar.CommitOpen (CommitOpen (..))
+import ZkFold.Base.Protocol.ARK.Protostar.SpecialSound (Input, ProverMessage)
+
+class AccumulatorScheme f c m a where
+  commit   :: a -> m -> c
+
+  prover   :: a -> Accumulator f c m -> Accumulator f c m -> (Accumulator f c m, [f])
+
+  verifier :: a -> AccumulatorInstance f c
+           -> AccumulatorInstance f c
+           -> (AccumulatorInstance f c, [f])
+           -> Bool
+
+  decider  :: a -> Accumulator f c m -> Bool
+
+instance (Input a ~ f, ProverMessage a ~ m) => AccumulatorScheme f c m (FiatShamir (CommitOpen c a)) where
+  commit (FiatShamir (CommitOpen cm _) _) = cm
+
+  prover   = undefined
+
+  verifier = undefined
+
+  decider  = undefined
