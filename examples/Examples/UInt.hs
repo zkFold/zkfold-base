850043ab2b2a5ade4e5f8a9c1f58299149a2daa5
TurtlePU <sokolov.p64@gmail.com>
2024-03-16 10:19:46 +0300

    + UInt examples

---
+++ examples/Examples/UInt.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeApplications    #-}
+
+module Examples.UInt (exampleUIntAdd, exampleUIntMul) where
+
+import           Data.Data                                   (Proxy (Proxy))
+import           Data.Function                               (($))
+import           Data.List                                   ((++))
+import           Data.String                                 (String)
+import           GHC.TypeNats                                (KnownNat, natVal)
+import           System.IO                                   (IO, putStrLn)
+import           Text.Show                                   (show)
+
+import           ZkFold.Base.Algebra.Basic.Class             (AdditiveSemigroup (..), MultiplicativeSemigroup (..), Semiring)
+import           ZkFold.Base.Algebra.Basic.Field             (Zp)
+import           ZkFold.Base.Algebra.EllipticCurve.BLS12_381 (BLS12_381_Scalar)
+import           ZkFold.Symbolic.Compiler                    (ArithmeticCircuit, compileIO)
+import           ZkFold.Symbolic.Data.UInt                   (UInt)
+
+exampleUIntAdd :: forall n . KnownNat n => IO ()
+exampleUIntAdd = makeExample @n "+" "add" (+)
+
+exampleUIntMul :: forall n . KnownNat n => IO ()
+exampleUIntMul = makeExample @n "*" "mul" (*)
+
+makeExample :: forall n . KnownNat n => String -> String -> (forall a . Semiring (UInt n a) => UInt n a -> UInt n a -> UInt n a) -> IO ()
+makeExample shortName name op = do
+    let n = show $ natVal (Proxy @n)
+    putStrLn $ "\nExample: (" ++ shortName ++ ") operation on UInt" ++ n
+    let file = "compiled_scripts/uint" ++ n ++ "_" ++ name ++ ".json"
+    compileIO @(Zp BLS12_381_Scalar) file (op @(ArithmeticCircuit (Zp BLS12_381_Scalar)))
--- examples/Main.hs
+++ examples/Main.hs
@@ -1,23 +1,28 @@
+{-# LANGUAGE TypeApplications #-}
+
 module Main where
 
+import           Examples.Conditional (exampleConditional)
+import           Examples.Eq          (exampleEq)
+import           Examples.Fibonacci   (exampleFibonacci)
+import           Examples.LEQ         (exampleLEQ)
+import           Examples.MiMCHash    (exampleMiMC)
+import           Examples.ReverseList (exampleReverseList)
+import           Examples.UInt        (exampleUIntAdd, exampleUIntMul)
 import           Prelude
-import           System.Directory       (createDirectoryIfMissing)
-
-import           Examples.Conditional   (exampleConditional)
-import           Examples.Eq            (exampleEq)
-import           Examples.Fibonacci     (exampleFibonacci)
-import           Examples.LEQ           (exampleLEQ)
-import           Examples.MiMCHash      (exampleMiMC)
-import           Examples.ReverseList   (exampleReverseList)
+import           System.Directory     (createDirectoryIfMissing)
 
 main :: IO ()
 main = do
     createDirectoryIfMissing True "compiled_scripts"
-    
+
     exampleReverseList
     exampleEq
     exampleFibonacci
     exampleMiMC
     exampleLEQ
     exampleConditional
-    
+    exampleUIntAdd @32
+    exampleUIntAdd @500
+    exampleUIntMul @32
+    exampleUIntMul @500
