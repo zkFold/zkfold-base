58d18af0ede62d7edaba0a9c1a1c8b0171476c7e
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2024-04-01 17:38:25 +0200

    Modify the Conditional example

--- examples/Examples/Conditional.hs
+++ examples/Examples/Conditional.hs
@@ -2,25 +2,22 @@
 
 module Examples.Conditional (exampleConditional) where
 
-import           Prelude                                     (IO, Integer, putStrLn)
+import           Prelude                                     (IO, putStrLn)
 
-import           ZkFold.Base.Algebra.Basic.Class
 import           ZkFold.Base.Algebra.Basic.Field             (Zp)
 import           ZkFold.Base.Algebra.EllipticCurve.BLS12_381 (BLS12_381_Scalar)
 import           ZkFold.Symbolic.Data.Bool                   (Bool)
 import           ZkFold.Symbolic.Data.Conditional            (Conditional(..))
-import           ZkFold.Symbolic.Data.Eq                     (Eq(..))
 import           ZkFold.Symbolic.Compiler
-import           ZkFold.Symbolic.Types                       (Symbolic)
 
--- | if x == y then 5 else 3
-ifEq :: forall a . Symbolic a => a -> a -> a
-ifEq x y = bool @(Bool a) (fromConstant (3 :: Integer)) (fromConstant (5 :: Integer)) (x == y)
-          
+type F = Zp BLS12_381_Scalar
+type A = ArithmeticCircuit F
+type B = Bool A
+
 exampleConditional :: IO ()
 exampleConditional = do
-    let file = "compiled_scripts/if.json"
+    let file = "compiled_scripts/conditional.json"
 
-    putStrLn "\nExample: ifEq\n"
+    putStrLn "\nExample: conditional\n"
 
-    compileIO @(Zp BLS12_381_Scalar) file (ifEq @(ArithmeticCircuit (Zp BLS12_381_Scalar)))
+    compileIO @F file (bool @B @A)
