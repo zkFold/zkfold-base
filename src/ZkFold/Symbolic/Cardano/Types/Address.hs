af23e7c1a68b8cad617f46ad35b09a438581dab9
Murat Kasimov <iokasimov.m@gmail.com>
2024-04-01 14:28:48 +0700

    Rewrite Cardano's Transaction and Address implementation

--- src/ZkFold/Symbolic/Cardano/Types/Address.hs
+++ src/ZkFold/Symbolic/Cardano/Types/Address.hs
@@ -2,32 +2,31 @@
 
 module ZkFold.Symbolic.Cardano.Types.Address where
 
-import           Prelude                          (Eq (..), ($), error, otherwise, concat, return)
+
+import           Prelude                  hiding ((*), length, splitAt)
+import           Control.Monad.State.Lazy        (evalState, state)
 
 import           ZkFold.Base.Algebra.Basic.Class
 import           ZkFold.Symbolic.Compiler
-import           ZkFold.Prelude                  (length, take, drop)
+import           ZkFold.Prelude                  (length, splitAt)
 
 data Address x = Address
-    {
-        addrType      :: x,
-        addrPayCred   :: x,
-        addrStakeCred :: x
-    }
+    { addrType :: x
+    , addrPayCred :: x
+    , addrStakeCred :: x
+    } deriving Eq
 
 instance Arithmetizable a x => Arithmetizable a (Address x) where
-    arithmetize (Address addrType payCred stakeCred) = do
-        addrType'      <- arithmetize addrType
-        payCred'       <- arithmetize payCred
-        stakeCred'     <- arithmetize stakeCred
-        return $ concat [addrType', payCred', stakeCred']
-
-    restore address
-        | length address == typeSize @a @(Address x) =
-            let addrType'      = restore $ take (typeSize @a @x) address
-                payCred'       = restore $ take (typeSize @a @x) $ drop (typeSize @a @x) address
-                stakeCred'     = restore $ drop (2 * typeSize @a @x) address
-            in Address addrType' payCred' stakeCred'
-        | otherwise = error "restore Address: wrong number of arguments"
-
+    arithmetize (Address addrType payCred stakeCred) =
+        (\t p s -> t <> p <> s)
+            <$> arithmetize addrType
+            <*> arithmetize payCred
+            <*> arithmetize stakeCred
+    restore address =
+        if length address == typeSize @a @(Address x)
+        then flip evalState address $ Address
+            <$> do restore <$> do state . splitAt $ typeSize @a @x
+            <*> do restore <$> do state . splitAt $ typeSize @a @x
+            <*> do restore <$> do state . splitAt $ typeSize @a @x
+        else error "restore Address: wrong number of arguments"
     typeSize = 3 * typeSize @a @x
--- src/ZkFold/Symbolic/Cardano/Types/Tx.hs
+++ src/ZkFold/Symbolic/Cardano/Types/Tx.hs
@@ -2,30 +2,55 @@
 
 module ZkFold.Symbolic.Cardano.Types.Tx where
 
-import           Prelude                          (Eq (..), ($), error, otherwise)
+import           Prelude                        hiding ((*), (+), length, splitAt)
+import           Control.Monad.State.Lazy              (evalState, state)
 
+import           ZkFold.Base.Algebra.Basic.Class
+import           ZkFold.Base.Data.Vector
 import           ZkFold.Symbolic.Compiler
-import           ZkFold.Prelude                  (length)
+import           ZkFold.Symbolic.Cardano.Types.Address
+import           ZkFold.Prelude                        (length, splitAt)
 
--- TODO (Issue #10): make a proper implementation
-newtype Tx x = Tx x
+data Transaction inputs outputs datum x = Transaction
+    { txId :: TxId x
+    , txInputs :: Vector inputs (Address x)
+    , txOutputs :: Vector outputs (Address x)
+    , txDatum :: datum x
+    } deriving Eq
 
-instance Arithmetizable a x => Arithmetizable a (Tx x) where
-    arithmetize (Tx x) = arithmetize x
+instance (Finite inputs, Finite outputs, Arithmetizable a x, Arithmetizable a (datum x))
+    => Arithmetizable a (Transaction inputs outputs datum x) where
 
-    restore tx
-        | length tx == typeSize @a @(Tx x) = Tx $ restore tx
-        | otherwise = error "restore Tx: wrong number of arguments"
+    arithmetize (Transaction tid inputs outputs datum) =
+        (\i is os d -> i <> is <> os <> d)
+            <$> arithmetize tid
+            <*> arithmetize inputs
+            <*> arithmetize outputs
+            <*> arithmetize datum
 
-    typeSize = typeSize @a @x
+    restore address =
+        if length address == typeSize @a @(Transaction inputs outputs datum x)
+        then flip evalState address $ Transaction
+            <$> do restore <$> do state . splitAt $ typeSize @a @(TxId x)
+            <*> do restore <$> do state . splitAt $ typeSize @a @(Vector inputs (Address x))
+            <*> do restore <$> do state . splitAt $ typeSize @a @(Vector outputs (Address x))
+            <*> do restore <$> do state . splitAt $ typeSize @a @(datum x)
+        else error "restore Transaction: wrong number of arguments"
+
+    typeSize = typeSize @a @(TxId x)
+             + typeSize @a @(Vector inputs (Address x))
+             + typeSize @a @(Vector outputs (Address x))
+             + typeSize @a @(datum x)
 
 newtype TxId x = TxId x
+    deriving Eq
 
 instance Arithmetizable a x => Arithmetizable a (TxId x) where
+
     arithmetize (TxId x) = arithmetize x
 
-    restore txId
-        | length txId == typeSize @a @(TxId x) = TxId $ restore txId
+    restore tx
+        | length tx == typeSize @a @(TxId x) = TxId $ restore tx
         | otherwise = error "restore TxId: wrong number of arguments"
 
     typeSize = typeSize @a @x
