35b7e8e8e8281f13e23fd747749eaf1055730e22
Murat Kasimov <iokasimov.m@gmail.com>
2024-04-08 12:53:43 +0700

    Make all types for Cardano to be newtype wrappers

--- src/ZkFold/Symbolic/Cardano/Types.hs
+++ src/ZkFold/Symbolic/Cardano/Types.hs
@@ -2,64 +2,34 @@
 module ZkFold.Symbolic.Cardano.Types where
 
 import           Prelude                        hiding ((*), (+), length, splitAt)
-import           Control.Monad.State.Lazy              (evalState, state)
 
 import           ZkFold.Symbolic.Data.UInt
 import           ZkFold.Symbolic.Data.UTCTime
 import           ZkFold.Base.Algebra.Basic.Class
 import           ZkFold.Base.Data.Vector
 import           ZkFold.Symbolic.Compiler
-import           ZkFold.Prelude                        (length, splitAt)
 
-data Transaction inputs rinputs outputs tokens datum x = Transaction
-    { txReferenceInputs :: Vector rinputs (Input datum x)
-    , txInputs :: Vector inputs (Input datum x)
-    , txOutputs :: Vector outputs (Output tokens x)
-    , txValidityInterval :: (UTCTime x, UTCTime x)
-    } deriving Eq
+newtype Transaction inputs rinputs outputs tokens datum a = Transaction
+    ( Vector rinputs (Input datum a)
+    , (Vector inputs (Input datum a)
+    , (Vector outputs (Output tokens a)
+    , (UTCTime a, UTCTime a)
+    ))) deriving Eq
 
-instance
+deriving instance
     ( Finite inputs
     , Finite rinputs
     , Finite outputs
     , Finite tokens
-    , Arithmetizable a x
-    , Arithmetizable a (datum x)
-    , Arithmetizable a (UInt 11 x)
-    , Arithmetizable a (UInt 32 x)
-    , Arithmetizable a (UInt 64 x)
-    ) => Arithmetizable a (Transaction inputs rinputs outputs tokens datum x) where
+    , Arithmetizable i a
+    , Arithmetizable i (datum a)
+    , Arithmetizable i (UInt 11 a)
+    , Arithmetizable i (UInt 32 a)
+    , Arithmetizable i (UInt 64 a)
+    ) => Arithmetizable i (Transaction inputs rinputs outputs tokens datum a)
 
-    arithmetize (Transaction inputs rinputs outputs vr) =
-        (\is ris os vr' -> is <> ris <> os <> vr')
-            <$> arithmetize inputs
-            <*> arithmetize rinputs
-            <*> arithmetize outputs
-            <*> arithmetize vr
-
-    restore address = flip evalState address $ Transaction
-        <$> do restore <$> do state . splitAt $ typeSize @a @(Vector inputs (Input datum x))
-        <*> do restore <$> do state . splitAt $ typeSize @a @(Vector rinputs (Input datum x))
-        <*> do restore <$> do state . splitAt $ typeSize @a @(Vector outputs (Output tokens x))
-        <*> do restore <$> do state . splitAt $ typeSize @a @(UTCTime x, UTCTime x)
-
-    typeSize = typeSize @a @(Vector inputs (Input datum x))
-             + typeSize @a @(Vector rinputs (Input datum x))
-             + typeSize @a @(Vector outputs (Output tokens x))
-             + typeSize @a @(UTCTime x, UTCTime x)
-
-newtype TxId x = TxId x
-    deriving Eq
-
-instance Arithmetizable a x => Arithmetizable a (TxId x) where
-
-    arithmetize (TxId x) = arithmetize x
-
-    restore tx
-        | length tx == typeSize @a @(TxId x) = TxId $ restore tx
-        | otherwise = error "restore TxId: wrong number of arguments"
-
-    typeSize = typeSize @a @x
+newtype TxId a = TxId a
+    deriving (Eq, Arithmetizable i)
 
 newtype Value n a = Value (Vector n ({- Bytes a-} a, ({- Bytes a -} a, UInt 64 a)))
     deriving Eq
@@ -68,100 +38,29 @@ deriving instance
     (Finite n, Arithmetizable i a, Arithmetizable i (UInt 64 a))
     => Arithmetizable i (Value n a)
 
-data Input datum a = Input
-    { txiOutputRef :: OutputRef a
-    -- TODO: should be `Script a`
-    , txiValidator :: a
-    , txiDatum :: datum a
-    -- TODO: should be sometning else
-    , txiRedeemer :: a
-    } deriving Eq
+newtype Input datum a = Input (OutputRef a, (a, datum a))
+    deriving Eq
 
-instance (Arithmetizable a x, Arithmetizable a (UInt 32 x), Arithmetizable a (datum x))
-    => Arithmetizable a (Input datum x) where
+deriving instance
+    (Arithmetizable i a, Arithmetizable i (UInt 32 a), Arithmetizable i (datum a))
+    => Arithmetizable i (Input datum a)
 
-    arithmetize (Input o v d r) =
-        (\o' v' d' r' -> o' <> v' <> d' <> r')
-            <$> arithmetize o
-            <*> arithmetize v
-            <*> arithmetize d
-            <*> arithmetize r
+newtype Output tokens a = Output (Address a, (Value tokens a, a))
+    deriving Eq
 
-    restore i = flip evalState i $ Input
-        <$> do restore <$> do state . splitAt $ typeSize @a @(OutputRef x)
-        <*> do restore <$> do state . splitAt $ typeSize @a @x
-        <*> do restore <$> do state . splitAt $ typeSize @a @(datum x)
-        <*> do restore <$> do state . splitAt $ typeSize @a @x
+deriving instance
+    (Finite tokens, Arithmetizable i a, Arithmetizable i (UInt 64 a))
+    => Arithmetizable i (Output tokens a)
 
-    typeSize = typeSize @a @(OutputRef x)
-             + typeSize @a @x
-             + typeSize @a @(datum x)
-             + typeSize @a @x
+newtype OutputRef a = OutputRef (TxId a, UInt 32 a)
+    deriving Eq
 
-data Output tokens a = Output
-    { txoAddress :: Address a
-    -- , txoValue :: UInt 64 a
-    , txiValue :: Value tokens a
-    -- TODO: replace with `Bytes x` or `DatumHash x`
-    , txoDatumHash :: a
-    } deriving Eq
+deriving instance
+    (Arithmetizable i a, Arithmetizable i (UInt 32 a))
+    => Arithmetizable i (OutputRef a)
 
-instance (Finite tokens, Arithmetizable a x, Arithmetizable a (UInt 64 x)) => Arithmetizable a (Output tokens x) where
-
-    arithmetize (Output o v d) =
-        (\o' v' d' -> o' <> v' <> d')
-            <$> arithmetize o
-            <*> arithmetize v
-            <*> arithmetize d
-
-    restore o = flip evalState o $ Output
-        <$> do restore <$> do state . splitAt $ typeSize @a @(Address x)
-        <*> do restore <$> do state . splitAt $ typeSize @a @(UInt 64 x)
-        <*> do restore <$> do state . splitAt $ typeSize @a @x
-
-    typeSize = typeSize @a @(Address x)
-             + typeSize @a @(UInt 64 x)
-             + typeSize @a @x
-
-data OutputRef a = OutputRef
-    { txoId :: TxId a
-    , txoIndex :: UInt 32 a
-    } deriving Eq
-
-instance (Arithmetizable a (UInt 32 x), Arithmetizable a x) => Arithmetizable a (OutputRef x) where
-
-    arithmetize (OutputRef txid index) = (<>)
-        <$> arithmetize txid
-        <*> arithmetize index
-
-    restore outputRef = flip evalState outputRef $ OutputRef
-        <$> do restore <$> do state . splitAt $ typeSize @a @(TxId x)
-        <*> do restore <$> do state . splitAt $ typeSize @a @(UInt 32 x)
-
-    typeSize = typeSize @a @(TxId x)
-             + typeSize @a @(UInt 32 x)
-
-data Address x = Address
-    { addrType :: x
-    , addrPayCred :: x
-    , addrStakeCred :: x
-    } deriving Eq
-
-instance Arithmetizable a x => Arithmetizable a (Address x) where
-    arithmetize (Address addrType payCred stakeCred) =
-        (\t p s -> t <> p <> s)
-            <$> arithmetize addrType
-            <*> arithmetize payCred
-            <*> arithmetize stakeCred
-    restore address = flip evalState address $ Address
-        <$> do restore <$> do state . splitAt $ typeSize @a @x
-        <*> do restore <$> do state . splitAt $ typeSize @a @x
-        <*> do restore <$> do state . splitAt $ typeSize @a @x
-    typeSize = 3 * typeSize @a @x
-
--- newtype Address x = Address (x, (x, x))
-    -- deriving Eq
-    -- deriving (Arithmetizable a) via (x, (x, x))
+newtype Address a = Address ({- Bytes a-} a, ({- Bytes a-} a, {- Bytes a-} a))
+    deriving (Eq, Arithmetizable i)
 
 newtype DatumHash datum a = DatumHash a
     deriving (Eq, Arithmetizable i)
