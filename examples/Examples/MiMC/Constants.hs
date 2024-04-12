72f46cd07826b97cb82502859819aa919a525102
Eitan Chatav <eitan.chatav@gmail.com>
2024-04-12 16:47:47 -0700

    little endian

--- examples/Examples/MiMC/Constants.hs
+++ examples/Examples/MiMC/Constants.hs
@@ -10,10 +10,12 @@ import           ZkFold.Base.Algebra.Basic.Class (FromConstant (..))
 import           ZkFold.Base.Data.ByteString
 import           ZkFold.Symbolic.Types           (I)
 
-mimcSeed :: Integer
+mimcSeed :: LittleEndian
 mimcSeed = 42
 
 mimcConstants :: forall a . (FromConstant I a) => [a]
 mimcConstants =
-  let cs = take 218 $ map (fromJust . fromByteString) $ iterate hash $ toByteString mimcSeed
+  let
+    getI = fromIntegral . fromJust . fromByteString @LittleEndian
+    cs = take 218 $ map getI $ iterate hash $ toByteString mimcSeed
   in map (fromConstant @I @a) (0 : cs ++ [0])
--- src/ZkFold/Base/Data/ByteString.hs
+++ src/ZkFold/Base/Data/ByteString.hs
@@ -1,16 +1,22 @@
+{-# LANGUAGE DerivingStrategies #-}
+
 module ZkFold.Base.Data.ByteString
   ( Binary (..)
   , toByteString
   , fromByteString
   , putWord8
   , getWord8
+  , LittleEndian (..)
   ) where
 
+import           Control.Applicative                         (many)
 import           Data.Binary
 import           Data.Binary.Get
 import           Data.Binary.Put
 import qualified Data.ByteString      as Strict
 import qualified Data.ByteString.Lazy as Lazy
+import           Data.Foldable                               (foldl')
+import           Numeric.Natural                             (Natural)
 import           Prelude
 
 toByteString :: Binary a => a -> Strict.ByteString
@@ -21,3 +27,19 @@ fromByteString x = case runGetOrFail get (Lazy.fromStrict x) of
   Left _ -> Nothing
   Right (leftover, _, a) ->
     if Lazy.null leftover then Just a else Nothing
+
+-- un little, deux little, trois little endians
+newtype LittleEndian = LittleEndian {unLittleEndian :: Natural}
+  deriving stock (Read, Show)
+  deriving newtype (Eq, Ord, Num, Enum, Real, Integral)
+instance Binary LittleEndian where
+  get = do
+    ns <- many getWord8
+    let accum n w8 = n * 256 + fromIntegral w8
+        littleEndian = LittleEndian (foldl' accum 0 ns)
+    return littleEndian
+  put (LittleEndian n)
+    | n == 0 = mempty
+    | otherwise =
+      let (n', r) = n `divMod` 256
+      in putWord8 (fromIntegral r) <> put (LittleEndian n')
