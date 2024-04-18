dbdd299fa2c8fb3e59ca38e3db925dc3474f4727
Eitan Chatav <eitan.chatav@gmail.com>
2024-04-18 12:48:32 -0700

    value erasure

--- src/ZkFold/Base/Algebra/Basic/Number.hs
+++ src/ZkFold/Base/Algebra/Basic/Number.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE MagicHash           #-}
 {-# LANGUAGE TypeApplications    #-}
 
 module ZkFold.Base.Algebra.Basic.Number (KnownNat, Prime, value, type (*), type (+), type (-), type (^)) where
 
-import           Data.Data    (Proxy (Proxy))
-import           GHC.TypeNats (KnownNat, Natural, natVal, type (*), type (+), type (-), type (^))
+import           GHC.Exts     (proxy#)
+import           GHC.TypeNats (KnownNat, Natural, natVal', type (*), type (+), type (-), type (^))
 
 class KnownNat p => Prime p
 
 value :: forall n . KnownNat n => Natural
-value = natVal (Proxy @n)
+-- argument erasure using
+-- proxy# :: Proxy# (n :: Natural) :: ZeroBitType
+value = natVal' (proxy# @n)
