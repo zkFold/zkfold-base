4467520cdafdd5cd837b4410ba92ffd56042d28c
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2024-04-21 13:16:55 +0200

    Hide the number of rounds in MiMC from the user

--- src/ZkFold/Symbolic/Algorithms/Hash/MiMC.hs
+++ src/ZkFold/Symbolic/Algorithms/Hash/MiMC.hs
@@ -2,18 +2,17 @@ module ZkFold.Symbolic.Algorithms.Hash.MiMC (mimcHash) where
 
 import           Numeric.Natural                                (Natural)
 import           Prelude                                        hiding (Eq (..), Num (..), any, not, (!!), (/), (^),
-                                                                 (||))
+                                                                 (||), length)
 
 import           ZkFold.Base.Algebra.Basic.Class
-import           ZkFold.Prelude                                 ((!!))
-import           ZkFold.Symbolic.Algorithms.Hash.MiMC.Constants (mimcConstants)
+import           ZkFold.Prelude                                 (length)
 import           ZkFold.Symbolic.Data.Conditional               (bool)
 import           ZkFold.Symbolic.Types                          (Symbolic)
 
 -- | MiMC-2n/n (Feistel) hash function.
 -- See https://eprint.iacr.org/2016/492.pdf, page 5
-mimcHash :: forall a . Symbolic a => Natural -> a -> a -> a -> a
-mimcHash nRounds k xL xR =
-    let c  = mimcConstants !! (nRounds-!1)
+mimcHash :: forall a . Symbolic a => [a] -> a -> a -> a -> a
+mimcHash cs k xL xR =
+    let c  = last cs
         t5 = (xL + k + c) ^ (5 :: Natural)
-    in bool (xR + t5) (mimcHash (nRounds-!1) k (xR + t5) xL) (nRounds > 1)
+    in bool (xR + t5) (mimcHash (init cs) k (xR + t5) xL) (length cs > 1)
