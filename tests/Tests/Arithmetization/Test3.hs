b407ab4118422cc28daed3658f24d330583328b0
echatav <echatav@users.noreply.github.com>
2024-04-27 08:30:30 +0000

    stylish-haskell auto-commit

--- tests/Tests/Arithmetization/Test3.hs
+++ tests/Tests/Arithmetization/Test3.hs
@@ -2,15 +2,15 @@
 
 module Tests.Arithmetization.Test3 (specArithmetization3) where
 
-import           Prelude                          hiding (Bool, Eq (..), Num (..), Ord (..), any, not, replicate, (/),
-                                                   (^), (||))
+import           Prelude                         hiding (Bool, Eq (..), Num (..), Ord (..), any, not, replicate, (/),
+                                                  (^), (||))
 import           Test.Hspec
 
-import           ZkFold.Base.Algebra.Basic.Field  (Zp)
+import           ZkFold.Base.Algebra.Basic.Field (Zp)
 import           ZkFold.Symbolic.Compiler
-import           ZkFold.Symbolic.Data.Bool        (Bool (..))
-import           ZkFold.Symbolic.Data.Ord         (Ord (..))
-import           ZkFold.Symbolic.Types            (Symbolic)
+import           ZkFold.Symbolic.Data.Bool       (Bool (..))
+import           ZkFold.Symbolic.Data.Ord        (Ord (..))
+import           ZkFold.Symbolic.Types           (Symbolic)
 
 type R = ArithmeticCircuit (Zp 97)
 
