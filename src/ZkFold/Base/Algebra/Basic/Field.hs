0b0978d4d835d8728c2560ce478f65b35986032d
Eitan Chatav <eitan.chatav@gmail.com>
2024-05-20 09:41:31 -0700

    use toZp in Binary (Zp p)

--- src/ZkFold/Base/Algebra/Basic/Field.hs
+++ src/ZkFold/Base/Algebra/Basic/Field.hs
@@ -152,7 +152,9 @@ instance ToJSON (Zp p) where
 instance FromJSON (Zp p) where
     parseJSON = fmap Zp . parseJSON
 
-deriving newtype instance Binary (Zp p)
+instance KnownNat p => Binary (Zp p) where
+    put (Zp a) = put a
+    get = toZp <$> get
 
 instance KnownNat p => Arbitrary (Zp p) where
     arbitrary = toZp <$> chooseInteger (0, fromIntegral (value @p) - 1)
