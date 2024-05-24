767aa59dde44f1587eaa3637e0d7c3165b7837cb
Eitan Chatav <eitan.chatav@gmail.com>
2024-05-24 12:49:27 -0700

    Update Types.hs

--- src/ZkFold/Symbolic/Types.hs
+++ src/ZkFold/Symbolic/Types.hs
@@ -1,5 +1,11 @@
-module ZkFold.Symbolic.Types (Symbolic) where
+module ZkFold.Symbolic.Types (Symbolic, SymbolicData, SymbolicFunction) where
 
-import           ZkFold.Base.Algebra.Basic.Class  (BinaryExpansion, FiniteField, TrichotomyField)
+import           Prelude                               (Traversable)
+import           ZkFold.Base.Algebra.Basic.Class       (BinaryExpansion, FiniteField, TrichotomyField)
+import           ZkFold.Base.Algebra.Basic.VectorSpace (FunctionSpace, VectorSpace, type InputSpace, type OutputSpace)
 
 type Symbolic a = (FiniteField a, BinaryExpansion a, TrichotomyField a)
+
+type SymbolicData a u = (VectorSpace a u, Traversable u)
+
+type SymbolicFunction a f = (FunctionSpace a f, Traversable (InputSpace a f), Traversable (OutputSpace a f))
