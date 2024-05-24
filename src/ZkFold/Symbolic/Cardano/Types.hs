4db1deda018071791e119eefe59c4730bb206581
Eitan Chatav <eitan.chatav@gmail.com>
2024-05-24 12:09:11 -0700

    Update Types.hs

--- src/ZkFold/Symbolic/Cardano/Types.hs
+++ src/ZkFold/Symbolic/Cardano/Types.hs
@@ -1,84 +1,154 @@
 {-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 module ZkFold.Symbolic.Cardano.Types where
 
+import           GHC.Generics                       hiding (UInt)
+import           Data.Functor.Identity
 import           Prelude                            hiding (Bool, Eq, length, splitAt, (*), (+))
+import qualified Prelude
 
+import           ZkFold.Base.Algebra.Basic.Class
 import           ZkFold.Base.Algebra.Basic.Number
+import           ZkFold.Base.Algebra.Basic.VectorSpace
 import           ZkFold.Base.Data.Vector
-import           ZkFold.Symbolic.Compiler
-import           ZkFold.Symbolic.Data.Bool          (Bool)
+import           ZkFold.Symbolic.Data.Bool
 import           ZkFold.Symbolic.Data.ByteString
-import           ZkFold.Symbolic.Data.Eq            (Eq)
-import           ZkFold.Symbolic.Data.Eq.Structural
 import           ZkFold.Symbolic.Data.UInt
 import           ZkFold.Symbolic.Data.UTCTime
 
 newtype Transaction inputs rinputs outputs tokens datum a = Transaction
-    ( Vector rinputs (Input tokens datum a)
-    , (Vector inputs (Input tokens datum a)
-    , (Vector outputs (Output tokens datum a)
-    , (UTCTime a, UTCTime a)
-    )))
+    ( (   (Vector rinputs :.: Input tokens datum)
+      :*: (Vector inputs :.: Input tokens datum)
+      :*: (Vector outputs :.: Output tokens datum)
+      :*: UTCTime :*: UTCTime
+      ) a
+    )
+    deriving stock
+        ( Prelude.Eq
+        , Prelude.Show
+        , Prelude.Functor
+        , Prelude.Foldable
+        , Prelude.Traversable
+        )
+deriving newtype instance
+  ( KnownNat rinputs
+  , KnownNat inputs
+  , KnownNat outputs
+  , KnownNat tokens
+  , FiniteField a
+  ) => VectorSpace a (Transaction inputs rinputs outputs tokens datum)
+instance 
+  ( KnownNat rinputs
+  , KnownNat inputs
+  , KnownNat outputs
+  , KnownNat tokens
+  , FiniteField a
+  ) => Eq a (Transaction inputs rinputs outputs tokens datum)
 
-deriving instance
-    ( Arithmetic a
-    , KnownNat inputs
-    , KnownNat rinputs
-    , KnownNat outputs
-    , KnownNat tokens
-    ) => SymbolicData a (Transaction inputs rinputs outputs tokens datum (ArithmeticCircuit a))
+txInputs :: Transaction inputs rinputs outputs tokens datum a -> (Vector inputs :.: Input tokens datum) a
+txInputs (Transaction (_ :*: is :*: _)) = is
 
-txInputs :: Transaction inputs rinputs outputs tokens datum a -> Vector inputs (Input tokens datum a)
-txInputs (Transaction (_, (is, _))) = is
-
-txOutputs :: Transaction inputs rinputs outputs tokens datum a -> Vector outputs (Output tokens datum a)
-txOutputs (Transaction (_, (_, (os, _)))) = os
+txOutputs :: Transaction inputs rinputs outputs tokens datum a -> (Vector outputs :.: Output tokens datum) a
+txOutputs (Transaction (_ :*: _ :*: os :*: _)) = os
 
 newtype TxId a = TxId a
+    deriving stock
+        ( Prelude.Eq
+        , Prelude.Show
+        , Prelude.Functor
+        , Prelude.Foldable
+        , Prelude.Traversable
+        )
+deriving via Identity instance VectorSpace a TxId
+instance Eq a TxId
 
-deriving instance Arithmetic a => SymbolicData a (TxId (ArithmeticCircuit a))
+newtype Value n a = Value ((Vector n :.: (ByteString 224 :*: ByteString 256 :*: UInt 64)) a)
+    deriving stock
+        ( Prelude.Eq
+        , Prelude.Show
+        , Prelude.Functor
+        , Prelude.Foldable
+        , Prelude.Traversable
+        )
+deriving newtype instance (KnownNat n, FiniteField a) => VectorSpace a (Value n)
+instance (KnownNat n, FiniteField a) => Eq a (Value n)
 
-newtype Value n a = Value (Vector n (ByteString 224 a, (ByteString 256 a, UInt 64 a)))
-
-deriving instance (Arithmetic a, KnownNat n) => SymbolicData a (Value n (ArithmeticCircuit a))
-
-newtype Input tokens datum a = Input (OutputRef a, Output tokens datum a)
+newtype Input tokens datum a = Input ((OutputRef :*: Output tokens datum) a)
+    deriving stock
+        ( Prelude.Eq
+        , Prelude.Show
+        , Prelude.Functor
+        , Prelude.Foldable
+        , Prelude.Traversable
+        )
+deriving newtype instance (KnownNat tokens, FiniteField a) => VectorSpace a (Input tokens datum)
+instance KnownNat tokens => Eq a (Input tokens datum)
 
 txiOutput :: Input tokens datum a -> Output tokens datum a
-txiOutput (Input (_, txo)) = txo
+txiOutput (Input (_ :*: txo)) = txo
 
 txiDatumHash :: Input tokens datum a -> ByteString 256 a
-txiDatumHash (Input (_, txo))= txoDatumHash txo
+txiDatumHash (Input (_ :*: txo)) = txoDatumHash txo
 
-deriving instance (Arithmetic a, KnownNat tokens) => SymbolicData a (Input tokens datum (ArithmeticCircuit a))
-
-newtype Output tokens datum a = Output (Address a, (Value tokens a, ByteString 256 a))
+newtype Output tokens datum a = Output ((Address :*: Value tokens :*: ByteString 256) a)
+    deriving stock
+        ( Prelude.Eq
+        , Prelude.Show
+        , Prelude.Functor
+        , Prelude.Foldable
+        , Prelude.Traversable
+        )
+deriving newtype instance (KnownNat tokens, FiniteField a) => VectorSpace a (Output tokens datum)
+instance KnownNat tokens => Eq a (Output tokens datum)
 
 txoAddress :: Output tokens datum a -> Address a
-txoAddress (Output (addr, _)) = addr
+txoAddress (Output (addr :*: _)) = addr
 
 txoDatumHash :: Output tokens datum a -> ByteString 256 a
-txoDatumHash (Output (_, (_, dh))) = dh
+txoDatumHash (Output (_ :*: _ :*: dh)) = dh
 
-deriving instance (Arithmetic a, KnownNat tokens) => SymbolicData a (Output tokens datum (ArithmeticCircuit a))
+newtype OutputRef a = OutputRef ((TxId :*: UInt 32) a)
+    deriving stock
+        ( Prelude.Eq
+        , Prelude.Show
+        , Prelude.Functor
+        , Prelude.Foldable
+        , Prelude.Traversable
+        )
+deriving newtype instance FiniteField a => VectorSpace a OutputRef
+instance Eq a OutputRef
 
-deriving via (Structural (Output tokens datum (ArithmeticCircuit a)))
-         instance (Arithmetic a, KnownNat tokens) =>
-         Eq (Bool (ArithmeticCircuit a)) (Output tokens datum (ArithmeticCircuit a))
-
-newtype OutputRef a = OutputRef (TxId a, UInt 32 a)
-
-deriving instance Arithmetic a => SymbolicData a (OutputRef (ArithmeticCircuit a))
-
-newtype Address a = Address (ByteString 4 a, (ByteString 224 a, ByteString 224 a))
+newtype Address a = Address ((ByteString 4 :*: ByteString 224 :*: ByteString 224) a)
+    deriving stock
+        ( Prelude.Eq
+        , Prelude.Show
+        , Prelude.Functor
+        , Prelude.Foldable
+        , Prelude.Traversable
+        )
+deriving newtype instance FiniteField a => VectorSpace a Address
+instance Eq a Address
 
 paymentCredential :: Address a -> ByteString 224 a
-paymentCredential (Address (_, (pc, _))) = pc
-
-deriving instance Arithmetic a => SymbolicData a (Address (ArithmeticCircuit a))
+paymentCredential (Address (_ :*: pc :*: _)) = pc
 
 newtype DatumHash datum a = DatumHash a
-    deriving (SymbolicData i)
+    deriving stock
+        ( Prelude.Eq
+        , Prelude.Show
+        , Prelude.Functor
+        , Prelude.Foldable
+        , Prelude.Traversable
+        )
+deriving via Identity instance VectorSpace a (DatumHash datum)
 
 newtype ScriptHash a = ScriptHash a
-    deriving (SymbolicData i)
+    deriving stock
+        ( Prelude.Eq
+        , Prelude.Show
+        , Prelude.Functor
+        , Prelude.Foldable
+        , Prelude.Traversable
+        )
+deriving via Identity instance VectorSpace a ScriptHash
