ab8b9bdf72a74ee5ca2ba1b0c3761459d0d3abc0
Eitan Chatav <eitan.chatav@gmail.com>
2024-05-24 10:21:10 -0700

    Update VectorSpace.hs

--- src/ZkFold/Base/Algebra/Basic/VectorSpace.hs
+++ src/ZkFold/Base/Algebra/Basic/VectorSpace.hs
@@ -78,12 +78,9 @@ instance (Generic1 v, VectorSpace a (Rep1 v))
     indexV (Generically1 v) i = indexV (from1 v) i
     tabulateV f = Generically1 (to1 (tabulateV f))
 
-instance VectorSpace a v => VectorSpace a (M1 i c v) where
-    type Basis a (M1 i c v) = Basis a v
-    indexV (M1 v) = indexV v
-    tabulateV f = M1 (tabulateV f)
-
 deriving via Representably Par1 instance VectorSpace a Par1
+deriving newtype instance VectorSpace a v => VectorSpace a (M1 i c v)
+deriving newtype instance VectorSpace a v => VectorSpace a (Rec1 v)
 
 -- zero dimensional vector space
 deriving via Representably U1 instance VectorSpace a U1
