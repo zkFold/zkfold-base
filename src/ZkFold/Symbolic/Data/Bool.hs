41acc7c3b60f2a9ff78c9b9721fda2c34cd64385
Eitan Chatav <eitan.chatav@gmail.com>
2024-05-24 11:24:46 -0700

    Boolean

--- src/ZkFold/Symbolic/Data/Bool.hs
+++ src/ZkFold/Symbolic/Data/Bool.hs
@@ -5,13 +5,8 @@ DerivingStrategies
 #-}
 
 module ZkFold.Symbolic.Data.Bool (
-    Bool(Bool),
-    true,
-    false,
-    not,
-    (&&),
-    (||),
-    xor,
+    Bool (Bool),
+    Boolean (..),
     bool,
     ifThenElse,
     (?),
@@ -37,25 +32,23 @@ deriving via Identity instance VectorSpace a Bool
 instance (Symbolic a, Haskell.Eq a) => Haskell.Show (Bool a) where
     show (Bool a) = if a Haskell.== one then "true" else "false"
 
-true :: Symbolic a => Bool a
-true = Bool one
+class Boolean a b where
+  true :: b a
+  false :: b a
+  not :: b a -> b a
+  infixr 3 &&
+  (&&) :: b a -> b a -> b a
+  infixr 2 ||
+  (||) :: b a -> b a -> b a
+  xor :: b a -> b a -> b a
 
-false :: Symbolic a => Bool a
-false = Bool zero
-
-not :: Symbolic a => Bool a -> Bool a
-not (Bool b) = Bool (one - b)
-
-infixr 3 &&
-(&&) :: Symbolic a => Bool a -> Bool a -> Bool a
-Bool b1 && Bool b2 = Bool (b1 * b2)
-
-infixr 2 ||
-(||) :: Symbolic a => Bool a -> Bool a -> Bool a
-Bool b1 || Bool b2 = Bool (b1 + b2 - b1 * b2)
-
-xor :: Symbolic a => Bool a -> Bool a -> Bool a
-xor (Bool b1) (Bool b2) = Bool (b1 + b2 - (b1 * b2 + b1 * b2))
+instance Symbolic a => Boolean a Bool where
+  true = Bool one
+  false = Bool zero
+  not (Bool b) = Bool (one - b)
+  Bool b1 && Bool b2 = Bool (b1 * b2)
+  Bool b1 || Bool b2 = Bool (b1 + b2 - b1 * b2)
+  xor (Bool b1) (Bool b2) = Bool (b1 + b2 - (b1 * b2 + b1 * b2))
 
 bool :: (Symbolic a, VectorSpace a u) => u a -> u a -> Bool a -> u a
 bool f t (Bool b) = scaleV b (t `subtractV` f) `addV` f
@@ -65,9 +58,12 @@ ifThenElse, (?)
 ifThenElse b t f = bool f t b
 (?) = ifThenElse
 
-class (VectorSpace a u, Haskell.Foldable u) => Eq a u where
+class Eq a u where
     infix 4 ==
     (==) :: Symbolic a => u a -> u a -> Bool a
+    default (==)
+      :: (Symbolic a, Haskell.Foldable u, VectorSpace a u)
+      => u a -> u a -> Bool a
     u == v = Bool (Haskell.foldl (*) one (zipWithV equal u v))
 instance Eq a Bool
 
