75af24086bf67f0f7bfd3655e10fbc366de821f2
vlasin <vlasin@users.noreply.github.com>
2024-06-23 10:24:15 +0000

    stylish-haskell auto-commit

--- src/ZkFold/Symbolic/Cardano/Contracts/RandomOracle.hs
+++ src/ZkFold/Symbolic/Cardano/Contracts/RandomOracle.hs
@@ -1,7 +1,7 @@
 module ZkFold.Symbolic.Cardano.Contracts.RandomOracle where
 
-import           Prelude                                        hiding (Bool, Eq (..), all, length, splitAt, zip, (&&), (==),
-                                                                 (*), (+), (!!), maybe)
+import           Prelude                                        hiding (Bool, Eq (..), all, length, maybe, splitAt, zip,
+                                                                 (!!), (&&), (*), (+), (==))
 
 import           ZkFold.Base.Algebra.Basic.Class
 import           ZkFold.Base.Data.Vector                        ((!!))
