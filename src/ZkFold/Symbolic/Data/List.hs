947f86fe2db27556eff81c76a7ca0f34af10a534
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2024-07-09 20:48:29 +0200

    Fix symbolic list definition

--- src/ZkFold/Symbolic/Data/List.hs
+++ src/ZkFold/Symbolic/Data/List.hs
@@ -2,28 +2,31 @@
 
 module ZkFold.Symbolic.Data.List where
 
+import           Data.Kind                                              (Type)
+import           GHC.TypeNats                                           (Natural)
 import           Prelude                                                (undefined)
 
 import           ZkFold.Symbolic.Compiler.ArithmeticCircuit.Instance    ()
 import           ZkFold.Symbolic.Data.Bool
+import           ZkFold.Symbolic.Data.FieldElement                      (FieldElementData(..))
 
-newtype List context a = List (context a)
+data List (context :: Natural -> Type -> Type) (a :: Type) x = List (context (TypeSize a context x) a) (context 1 a)
 
 infixr 5 .:
-(.:) :: List context a -> x -> List context a
+(.:) :: List context a x -> x -> List context a x
 _ .: _ = undefined
 
-uncons :: List context a -> (x, List context a)
+uncons :: List context a x -> (x, List context a x)
 uncons = undefined
 
-head :: List context a -> x
+head :: List context a x -> x
 head xs = let (x, _) = uncons xs in x
 
-tail :: List context a -> List context a
+tail :: List context a x -> List context a x
 tail xs = let (_, xs') = uncons xs in xs'
 
-null :: List context a -> Bool (context a)
+null :: List context a x -> Bool (context 1 a)
 null = undefined
 
-foldl :: (x -> a -> x) -> x -> List context a -> x
+foldl :: (x -> a -> x) -> x -> List context a x -> x
 foldl = undefined
