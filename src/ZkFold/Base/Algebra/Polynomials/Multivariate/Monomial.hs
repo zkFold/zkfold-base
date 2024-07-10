5459ca8bd07890749bdd37d7f14f3ad3aef61bca
vlasin <vlasin@users.noreply.github.com>
2024-07-10 16:35:13 +0000

    stylish-haskell auto-commit

--- src/ZkFold/Base/Algebra/Polynomials/Multivariate/Monomial.hs
+++ src/ZkFold/Base/Algebra/Polynomials/Multivariate/Monomial.hs
@@ -7,8 +7,8 @@ module ZkFold.Base.Algebra.Polynomials.Multivariate.Monomial where
 import           Control.DeepSeq                 (NFData)
 import           Data.Aeson                      (FromJSON, ToJSON)
 import           Data.List                       (intercalate)
-import           Data.Map.Strict                 (Map, differenceWith, empty, filter, foldrWithKey, isSubmapOfBy,
-                                                  lookup, mapKeys, unionWith, fromListWith)
+import           Data.Map.Strict                 (Map, differenceWith, empty, filter, foldrWithKey, fromListWith,
+                                                  isSubmapOfBy, lookup, mapKeys, unionWith)
 import qualified Data.Map.Strict                 as Map
 import           GHC.Generics                    (Generic)
 import           GHC.IsList                      (IsList (..))
--- src/ZkFold/Symbolic/Compiler/ArithmeticCircuit/Instance.hs
+++ src/ZkFold/Symbolic/Compiler/ArithmeticCircuit/Instance.hs
@@ -22,7 +22,8 @@ import           Prelude                                                   (Inte
                                                                             (++), (.), (<$>), (>>=))
 import qualified Prelude                                                   as Haskell
 import           System.Random                                             (mkStdGen)
-import           Test.QuickCheck                                           (Arbitrary (arbitrary), Gen, oneof, vector, chooseInteger)
+import           Test.QuickCheck                                           (Arbitrary (arbitrary), Gen, chooseInteger,
+                                                                            oneof, vector)
 
 import           ZkFold.Base.Algebra.Basic.Class
 import           ZkFold.Base.Algebra.Basic.Field                           (Zp)
