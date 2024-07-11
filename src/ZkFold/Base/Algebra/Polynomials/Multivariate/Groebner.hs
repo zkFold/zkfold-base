2671952024fe2f60252994bf45173ab8f2c2b3d8
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2024-07-11 10:43:34 +0200

    Add Groebner basis algorithm

--- src/ZkFold/Base/Algebra/Polynomials/Multivariate/Groebner.hs
+++ src/ZkFold/Base/Algebra/Polynomials/Multivariate/Groebner.hs
@@ -2,6 +2,7 @@ module ZkFold.Base.Algebra.Polynomials.Multivariate.Groebner where
 
 import           Data.Bool                                               (bool)
 import           Data.List                                               (sortBy)
+import           GHC.Natural                                             (Natural)
 import           Prelude                                                 hiding (Num (..), drop, lcm, length, sum, take,
                                                                           (!!), (/))
 
@@ -59,6 +60,11 @@ systemReduce = foldr f []
             let p' = fullReduceMany p ps
             in bool ps (p' : ps) (not $ zeroP p')
 
+data GroebnerParams c i j = GroebnerParams
+    { groebnerMaxSteps      :: Natural
+    , groebnerSPolySelector :: Poly c i j -> Bool
+    }
+
 makeSPoly ::
        (Ring j, Polynomial c i j)
     => Poly c i j
@@ -79,12 +85,33 @@ makeSPoly l r =
 
 groebnerStep ::
         (Ring j, Polynomial c i j)
-    => [Poly c i j]
+    => GroebnerParams c i j
     -> [Poly c i j]
-groebnerStep ps =
+    -> [Poly c i j]
+groebnerStep GroebnerParams{..} ps =
     let n = length ps
         inds = [(i, j) | i <- [0 .. n-!1], j <- [0 .. n-!1], i < j]
         ss   = map (\(i, j) -> makeSPoly (ps !! i) (ps !! j) `reduceMany` ps) inds
         ss'  = filter (not . zeroP) ss
-        ps'  = sortBy (flip compare) (ss' ++ ps)
+        ss'' = filter groebnerSPolySelector ss'
+        ps'  = sortBy (flip compare) (ss'' ++ ps)
     in systemReduce ps'
+
+groebner ::
+       forall c i j . (Ring j, Polynomial c i j)
+    => GroebnerParams c i j
+    -> [Poly c i j]
+    -> [Poly c i j]
+groebner GroebnerParams{..} ps =
+    let step = groebnerStep GroebnerParams{..}
+        go 0 ps' = ps'
+        go n ps' = go (n -! 1) $ step ps'
+    in go groebnerMaxSteps ps
+
+verifyGroebner ::
+       forall c i j . (Ring j, Polynomial c i j)
+    => GroebnerParams c i j
+    -> [Poly c i j]
+    -> Poly c i j
+    -> Bool
+verifyGroebner params ps = zeroP . (`fullReduceMany` groebner params ps)
