509c73a18c8aae006915ff31b3af39d87e3f1a58
zlonast <zlonast3@gmail.com>
2024-08-05 20:59:56 +0000

    add safeZero

--- src/ZkFold/Symbolic/Compiler/ArithmeticCircuit/Combinators.hs
+++ src/ZkFold/Symbolic/Compiler/ArithmeticCircuit/Combinators.hs
@@ -11,6 +11,7 @@ module ZkFold.Symbolic.Compiler.ArithmeticCircuit.Combinators (
     splitExpansion,
     horner,
     desugarRange,
+    safeZero,
     isZeroC,
     invertC,
     foldCircuit,
@@ -119,6 +120,11 @@ desugarRange i b
   where forceGE j c k
           | c == zero = ($ j) * (one - ($ k))
           | otherwise = one + ($ k) * (($ j) - one)
+
+safeZero :: (Arithmetic a, Traversable f) => ArithmeticCircuit a f -> ArithmeticCircuit a f
+safeZero r = circuitF $ do
+    is' <- runCircuit r
+    for is' $ \i -> newConstrained (\x j -> (x i - one) * x j) (\x -> x i - one)
 
 isZeroC :: (Arithmetic a, Z.Zip f, Traversable f) => ArithmeticCircuit a f -> ArithmeticCircuit a f
 isZeroC r = circuitF $ fst <$> runInvert r
