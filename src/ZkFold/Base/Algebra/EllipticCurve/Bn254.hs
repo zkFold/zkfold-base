ea10342e34183eb4fe6f89c8cd5e0b6c97df05bb
TurtlePU <sokolov.p64@gmail.com>
2024-08-08 22:07:09 +0300

    + bn254 complete definition

--- src/ZkFold/Base/Algebra/EllipticCurve/Bn254.hs
+++ src/ZkFold/Base/Algebra/EllipticCurve/Bn254.hs
@@ -1,21 +1,110 @@
+{-# LANGUAGE DerivingVia     #-}
+{-# LANGUAGE OverloadedLists #-}
+
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 module ZkFold.Base.Algebra.EllipticCurve.Bn254 where
 
+import           Data.Eq                                    (Eq)
+import           Prelude                                    (Integer)
+
 import           ZkFold.Base.Algebra.Basic.Class
-import           ZkFold.Base.Algebra.Basic.Field         (Zp)
+import           ZkFold.Base.Algebra.Basic.Field            (Ext2 (..), Ext3, IrreduciblePoly (..), Zp)
 import           ZkFold.Base.Algebra.Basic.Number
 import           ZkFold.Base.Algebra.EllipticCurve.Class
+import           ZkFold.Base.Algebra.EllipticCurve.Pairing  (ate)
+import           ZkFold.Base.Algebra.Polynomials.Univariate (Poly, toPoly)
 
-type Bn254_Base = 0x2523648240000001BA344D80000000086121000000000013A700000000000013
+-------------------------- Scalar field & field towers -------------------------
+
+type Bn254_Scalar = 21888242871839275222246405745257275088548364400416034343698204186575808495617
+instance Prime Bn254_Scalar
+
+type Bn254_Base = 21888242871839275222246405745257275088696311157297823662689037894645226208583
 instance Prime Bn254_Base
 
-data Bn254
+type Fr = Zp Bn254_Scalar
+type Fp = Zp Bn254_Base
 
-instance EllipticCurve Bn254 where
-  type BaseField Bn254 = Zp Bn254_Base
-  type ScalarField Bn254 = _
+x :: (Ring a, Eq a) => Poly a
+x = toPoly [zero, one]
 
-instance StandardEllipticCurve Bn254 where
+u :: Poly Fp
+u = x
+
+instance IrreduciblePoly Fp "IP1" where
+  irreduciblePoly = u ^ (2 :: Natural) + one
+
+type Fp2 = Ext2 Fp "IP1"
+
+v :: Poly Fp2
+v = x
+
+instance IrreduciblePoly Fp2 "IP2" where
+  irreduciblePoly = v ^ (3 :: Natural) - fromConstant (9 :: Natural) - fromConstant u
+
+type Fp6 = Ext3 Fp2 "IP2"
+
+w :: Poly Fp6
+w = x
+
+instance IrreduciblePoly Fp6 "IP3" where
+  irreduciblePoly = w ^ (2 :: Natural) - fromConstant v
+
+type Fp12 = Ext2 Fp6 "IP3"
+
+------------------------------- bn254 G1 ---------------------------------------
+
+data Bn254_G1
+
+instance EllipticCurve Bn254_G1 where
+  type ScalarField Bn254_G1 = Fr
+  type BaseField Bn254_G1 = Fp
+  inf = Inf
+  gen = Point 1 2
+  add = pointAdd
+  mul = pointMul
+
+instance StandardEllipticCurve Bn254_G1 where
+  aParameter = 0
+  bParameter = 3
+
+------------------------------- bn254 G2 ---------------------------------------
+
+data Bn254_G2
+
+instance EllipticCurve Bn254_G2 where
+  type ScalarField Bn254_G2 = Fr
+  type BaseField Bn254_G2 = Fp2
+  inf = Inf
+  gen = Point
+    (Ext2 10857046999023057135944570762232829481370756359578518086990519993285655852781
+          11559732032986387107991004021392285783925812861821192530917403151452391805634)
+    (Ext2 8495653923123431417604973247489272438418190587263600148770280649306958101930
+          4082367875863433681332203403145435568316851327593401208105741076214120093531)
+  add = pointAdd
+  mul = pointMul
+
+instance StandardEllipticCurve Bn254_G2 where
   aParameter = zero
-  bParameter = one + one
+  bParameter = Ext2 3 0 // Ext2 9 1
+
+------------------------------- Pairing ----------------------------------------
+
+newtype Bn254_GT = Bn254_GT Fp12
+  deriving (Eq, MultiplicativeSemigroup, MultiplicativeMonoid)
+
+instance Exponent Bn254_GT Natural where
+  Bn254_GT e ^ p = Bn254_GT (e ^ p)
+
+instance Exponent Bn254_GT Integer where
+  Bn254_GT e ^ p = Bn254_GT (e ^ p)
+
+deriving via (NonZero Fp12) instance MultiplicativeGroup Bn254_GT
+
+instance Finite Bn254_GT where
+  type Order Bn254_GT = Bn254_Scalar
+
+instance Pairing Bn254_G1 Bn254_G2 where
+  type TargetGroup Bn254_G1 Bn254_G2 = Bn254_GT
+  pairing p q = Bn254_GT (ate p q)
---
+++ src/ZkFold/Base/Algebra/EllipticCurve/Pairing.hs
@@ -0,0 +1,116 @@
+{-# LANGUAGE TypeOperators #-}
+
+module ZkFold.Base.Algebra.EllipticCurve.Pairing (ate) where
+
+import           Data.Bits                               (shiftR)
+import           Data.Bool                               (Bool, otherwise, (&&))
+import           Data.Eq                                 (Eq, (==))
+import           Data.Function                           (($))
+import           Data.List                               (reverse, tail, unfoldr)
+import           Data.Maybe                              (Maybe (..))
+import           Data.Ord                                ((<=))
+import           Data.Type.Equality                      (type (~))
+import           Numeric.Natural                         (Natural)
+import           Prelude                                 (Integer, error, even, odd)
+
+import           ZkFold.Base.Algebra.Basic.Class
+import           ZkFold.Base.Algebra.Basic.Field         (Ext2 (..), Ext3 (..))
+import           ZkFold.Base.Algebra.EllipticCurve.Class
+
+-- Adapted from:
+-- https://github.com/nccgroup/pairing-bls12381/blob/master/Crypto/Pairing_bls12381.hs
+
+type Untwisted c i1 i2 = Ext2 (Ext3 (BaseField c) i1) i2
+
+-- Untwist point on E2 for pairing calculation
+untwist ::
+  (Field (BaseField c), Untwisted c i1 i2 ~ g, Field g) => Point c -> (g, g)
+untwist (Point x1 y1) = (wideX, wideY)
+  where
+    root = Ext3 zero one zero
+    wideX = Ext2 zero (Ext3 zero zero x1) // Ext2 zero root
+    wideY = Ext2 zero (Ext3 zero zero y1) // Ext2 root zero
+untwist Inf = error "untwist: point at infinity"
+
+-- Used in miller loop for computing line functions l_r,r and v_2r
+doubleEval ::
+  Field (BaseField c2) => Untwisted c2 i1 i2 ~ g => 
+  FromConstant (BaseField c1) g => Field g =>
+  Point c2 -> Point c1 -> g
+doubleEval r (Point px py) = fromConstant py - (fromConstant px * slope) - v
+  where
+    (rx, ry) = untwist r
+    slope = (rx * rx + rx * rx + rx * rx) // (ry + ry)
+    v = ry - slope * rx
+doubleEval _ Inf = error "doubleEval: point at infinity"
+
+-- Used in miller loop for computer line function l_r,p and v_r+p
+addEval ::
+  (BaseField c2 ~ f, Field f, Eq f) =>
+  (Untwisted c2 i1 i2 ~ g, FromConstant (BaseField c1) g, Field g) =>
+  Point c2 -> Point c2 -> Point c1 -> g
+addEval r q p@(Point px _) = if (rx == qx) && (ry + qy == zero)
+                then fromConstant px - rx
+                else addEval' (rx, ry) (qx, qy) p
+  where
+    (rx, ry) = untwist r
+    (qx, qy) = untwist q
+addEval _ _ Inf = error "addEval: point at infinity"
+
+-- Helper function for addEval
+addEval' ::
+  (FromConstant (BaseField c) g, Field g) => (g, g) -> (g, g) -> Point c -> g
+addEval' (rx, ry) (qx, qy) (Point px py) =
+  fromConstant py - (fromConstant px * slope) - v
+  where
+    slope = (qy - ry) // (qx - rx)
+    v = ((qy * rx) - (ry * qx)) // (rx - qx)
+addEval' _ _ Inf = error "addEval': point at infinity"
+
+-- Classic Miller loop for Ate pairing
+miller ::
+  (BaseField c2 ~ f, Field f, Eq f) =>
+  (Untwisted c2 i1 i2 ~ g, FromConstant (BaseField c1) g, Field g) =>
+  Point c1 -> Point c2 -> g
+miller p q = miller' p q q iterations one
+  where
+    iterations = tail $ reverse $  -- list of true/false per bits of operand
+      unfoldr (\b -> if b == (0 :: Integer) then Nothing
+                     else Just(odd b, shiftR b 1)) 0xd201000000010000
+
+-- Double and add loop helper for Miller (iterative)
+miller' ::
+  (BaseField c2 ~ f, Field f, Eq f) =>
+  (Untwisted c2 i1 i2 ~ g, FromConstant (BaseField c1) g, Field g) =>
+  Point c1 -> Point c2 -> Point c2 -> [Bool] -> g -> g
+miller' _ _ _ [] result = result
+miller' p q r (i:iters) result =
+  if i then miller' p q (pointAdd doubleR q) iters (accum * addEval doubleR q p)
+       else miller' p q doubleR iters accum
+  where
+    accum = result * result * doubleEval r p
+    doubleR = pointDouble r
+
+-- | Pairing calculation for a valid point in G1 and another valid point in G2.
+-- Classic Ate pairing.
+ate ::
+  forall c1 c2 i1 i2 f g.
+  (Finite (ScalarField c2), BaseField c2 ~ f, FiniteField f, Eq f) =>
+  (Untwisted c2 i1 i2 ~ g, FromConstant (BaseField c1) g, Field g) =>
+  Point c1 -> Point c2 -> g
+ate Inf _ = zero
+ate _ Inf = zero
+ate p1 p2 = pow' (miller p1 p2) ((p ^ (12 :: Natural) -! 1) `div` r) one
+  where
+    p = order @(BaseField c2)
+    r = order @(ScalarField c2)
+
+-- Used for the final exponentiation; opportunity for further perf optimization
+pow' :: MultiplicativeSemigroup a => a -> Natural -> a -> a
+pow' a0 e result
+  | e <= 1    = a0
+  | even e    = accum2
+  | otherwise = accum2 * a0
+  where
+    accum  = pow' a0 (shiftR e 1) result
+    accum2 = accum * accum
