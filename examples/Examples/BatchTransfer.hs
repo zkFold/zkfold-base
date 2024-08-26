5b8260c449318a9cd647ddd98138d93655c170dd
hovanja2011 <A.Hovanskaya@zkfold.io>
2024-08-26 16:06:27 +0300

    exampleBatchTransfer example changed

--- examples/Examples/BatchTransfer.hs
+++ examples/Examples/BatchTransfer.hs
@@ -1,17 +1,9 @@
-{-# LANGUAGE TypeOperators #-}
-
 module Examples.BatchTransfer (exampleBatchTransfer) where
 
-import           Data.Type.Equality                              (type (~))
-
-import           ZkFold.Base.Algebra.Basic.Field                 (Zp)
-import           ZkFold.Base.Algebra.EllipticCurve.BLS12_381     (BLS12_381_Scalar)
 import           ZkFold.Base.Data.Vector                         (Vector)
-import           ZkFold.Symbolic.Cardano.Contracts.BatchTransfer (Tx, TxOut, batchTransfer)
+import           ZkFold.Symbolic.Cardano.Contracts.BatchTransfer (Tx, TxOut, batchTransfer, Sig)
 import           ZkFold.Symbolic.Cardano.Types                   (Bool, ByteString)
-import           ZkFold.Symbolic.Class                           (Symbolic (BaseField))
 
-type F = Zp BLS12_381_Scalar
 
-exampleBatchTransfer :: (Symbolic c, BaseField c ~ F) => Tx c -> Vector 5 (TxOut c, TxOut c, ByteString 256 c) -> Bool c
+exampleBatchTransfer :: (Sig c) => Tx c -> Vector 5 (TxOut c, TxOut c, ByteString 256 c) -> Bool c
 exampleBatchTransfer = batchTransfer
