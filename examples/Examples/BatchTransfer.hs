2e91657cb6d7d3c7473aeefe07992f0745009c06
hovanja2011 <hovanja2011@users.noreply.github.com>
2024-08-26 12:41:28 +0000

    stylish-haskell auto-commit

--- examples/Examples/BatchTransfer.hs
+++ examples/Examples/BatchTransfer.hs
@@ -4,12 +4,12 @@ module Examples.BatchTransfer (exampleBatchTransfer) where
 
 import           Data.Type.Equality                              (type (~))
 
+import           ZkFold.Base.Algebra.Basic.Field                 (Zp)
+import           ZkFold.Base.Algebra.EllipticCurve.BLS12_381     (BLS12_381_Scalar)
 import           ZkFold.Base.Data.Vector                         (Vector)
 import           ZkFold.Symbolic.Cardano.Contracts.BatchTransfer (Tx, TxOut, batchTransfer)
 import           ZkFold.Symbolic.Cardano.Types                   (Bool, ByteString)
 import           ZkFold.Symbolic.Class                           (Symbolic (BaseField))
-import ZkFold.Base.Algebra.EllipticCurve.BLS12_381 (BLS12_381_Scalar)
-import ZkFold.Base.Algebra.Basic.Field (Zp)
 
 type F = Zp BLS12_381_Scalar
 
--- src/ZkFold/Symbolic/Cardano/Types/Basic.hs
+++ src/ZkFold/Symbolic/Cardano/Types/Basic.hs
@@ -7,14 +7,14 @@ module ZkFold.Symbolic.Cardano.Types.Basic
     , Symbolic.UTCTime
     ) where
 
-import           Prelude                                     hiding (Bool, Eq, length, splitAt, (*), (+))
+import           Prelude                           hiding (Bool, Eq, length, splitAt, (*), (+))
 
-import           ZkFold.Base.Data.Vector                     (Vector)
-import qualified ZkFold.Symbolic.Data.Bool                   as Symbolic
-import qualified ZkFold.Symbolic.Data.ByteString             as Symbolic
-import qualified ZkFold.Symbolic.Data.FieldElement           as Symbolic
-import qualified ZkFold.Symbolic.Data.UInt                   as Symbolic
-import qualified ZkFold.Symbolic.Data.UTCTime                as Symbolic
+import           ZkFold.Base.Data.Vector           (Vector)
+import qualified ZkFold.Symbolic.Data.Bool         as Symbolic
+import qualified ZkFold.Symbolic.Data.ByteString   as Symbolic
+import qualified ZkFold.Symbolic.Data.FieldElement as Symbolic
+import qualified ZkFold.Symbolic.Data.UInt         as Symbolic
+import qualified ZkFold.Symbolic.Data.UTCTime      as Symbolic
 
 type FieldElement context     = Symbolic.FieldElement context
 type FieldElementBits context = context (Vector 256)
