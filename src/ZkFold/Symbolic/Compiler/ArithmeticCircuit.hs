2ba627d6b34d8d58c22c269b00854472a090681c
echatav <echatav@users.noreply.github.com>
2024-08-27 18:07:46 +0000

    stylish-haskell auto-commit

--- src/ZkFold/Symbolic/Compiler/ArithmeticCircuit.hs
+++ src/ZkFold/Symbolic/Compiler/ArithmeticCircuit.hs
@@ -52,8 +52,8 @@ import           ZkFold.Base.Algebra.Polynomials.Multivariate        (evalMonomi
 import           ZkFold.Prelude                                      (length)
 import           ZkFold.Symbolic.Compiler.ArithmeticCircuit.Instance ()
 import           ZkFold.Symbolic.Compiler.ArithmeticCircuit.Internal (Arithmetic, ArithmeticCircuit (..), Constraint,
-                                                                      Var (..), acInput, eval, eval1, exec,
-                                                                      exec1, witnessGenerator)
+                                                                      Var (..), acInput, eval, eval1, exec, exec1,
+                                                                      witnessGenerator)
 import           ZkFold.Symbolic.Compiler.ArithmeticCircuit.Map
 import           ZkFold.Symbolic.Data.Combinators                    (expansion)
 import           ZkFold.Symbolic.MonadCircuit                        (MonadCircuit (..))
