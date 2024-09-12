c63329c22ba24a9006ccc185fdc46d3f5541a42b
echatav <echatav@users.noreply.github.com>
2024-09-12 19:14:19 +0000

    stylish-haskell auto-commit

--- examples/Examples/Constant.hs
+++ examples/Examples/Constant.hs
@@ -2,10 +2,11 @@ module Examples.Constant (exampleConst5, exampleEq5) where
 
 import           Examples.Eq                       (exampleEq)
 import           Numeric.Natural                   (Natural)
+
+import           ZkFold.Base.Algebra.Basic.Class   (FromConstant (..))
 import           ZkFold.Symbolic.Class             (Symbolic)
 import           ZkFold.Symbolic.Data.Bool         (Bool)
 import           ZkFold.Symbolic.Data.FieldElement (FieldElement)
-import           ZkFold.Base.Algebra.Basic.Class   (FromConstant (..))
 
 exampleConst5 :: Symbolic c => FieldElement c
 exampleConst5 = fromConstant (5 :: Natural)
