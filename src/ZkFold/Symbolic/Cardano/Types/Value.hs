7a25de15e05d55d068f99d9f346e463cfbd79f9a
Eitan Chatav <eitan.chatav@gmail.com>
2024-09-30 11:02:56 -0700

    put instances back

--- src/ZkFold/Symbolic/Cardano/Types/Value.hs
+++ src/ZkFold/Symbolic/Cardano/Types/Value.hs
@@ -3,6 +3,7 @@
 
 module ZkFold.Symbolic.Cardano.Types.Value where
 
+import qualified Data.Map                            as Map
 import           GHC.Natural                         (Natural)
 import           Prelude                             hiding (Bool, Eq, length, replicate, splitAt, (*), (+))
 import qualified Prelude                             as Haskell
@@ -35,3 +36,16 @@ deriving instance
 
 instance Symbolic context => Scale Natural (Value n context) where
     n `scale` Value v = Value $ fmap (\((pid, aname), q) -> ((pid, aname), n `scale` q)) v
+
+instance (Haskell.Ord (PolicyId context), Haskell.Ord (AssetName context), Symbolic context) => Semigroup (Value n context) where
+    (<>) (Value va) (Value vb) = Value $ unsafeToVector $ Map.toList $ Map.unionWith (+) (Map.fromList (fromVector va)) (Map.fromList (fromVector vb))
+
+instance (Haskell.Ord (PolicyId context), Haskell.Ord (AssetName context), Symbolic context) => Monoid (Value n context) where
+    mempty = Value $ unsafeToVector []
+
+instance (Haskell.Ord (PolicyId context), Haskell.Ord (AssetName context), Symbolic context) => AdditiveSemigroup (Value n context) where
+    (+) = (<>)
+
+instance
+    (Haskell.Ord (PolicyId context), Haskell.Ord (AssetName context), Symbolic context) => AdditiveMonoid (Value n context) where
+    zero = mempty
