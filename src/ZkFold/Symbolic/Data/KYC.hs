145481dc07059c313dec5f85f466d2522de53bbd
charrsky <maxchrlv@gmail.com>
2024-09-30 20:25:05 +0300

    make an example

--- src/ZkFold/Symbolic/Data/KYC.hs
+++ src/ZkFold/Symbolic/Data/KYC.hs
@@ -1,18 +1,25 @@
 {-# LANGUAGE UndecidableInstances #-}
 module ZkFold.Symbolic.Data.KYC where
 
-import           Prelude                             hiding (Bool, Eq, length, replicate, splitAt, (*), (+))
-
-import           ZkFold.Symbolic.Cardano.Types.Basic
+import ZkFold.Symbolic.Cardano.Types.Basic
+    ( ByteString, UInt, Bool )
 import           ZkFold.Symbolic.Data.Combinators    (RegisterSize (..))
 import GHC.Generics (Generic)
+import ZkFold.Symbolic.Class (Symbolic)
+import ZkFold.Symbolic.Data.Bool (BoolType(..))
+import ZkFold.Base.Data.Vector ( parFmap, Vector )
+import ZkFold.Symbolic.Data.Eq ( Eq((==)) )
+import Data.Foldable ( Foldable(foldl') ) 
+
+
+type KYCByteString context = ByteString 256 context
 
 data KYCData context = KYCData 
-    { kycType :: ByteString 256 context
-    , kycValue :: ByteString 256 context
-    , kycHash :: ByteString 256 context
+    { kycType :: KYCByteString context
+    , kycValue :: KYCByteString context
+    , kycHash :: KYCByteString context
     , kycID :: UInt 64 Auto context
     } deriving Generic
 
-
-
+isCitizen :: (Symbolic c) => KYCByteString c -> Vector n (KYCByteString c) -> Bool c
+isCitizen citizenship allowedCountries = foldl' (||) false (parFmap (== citizenship) allowedCountries)
