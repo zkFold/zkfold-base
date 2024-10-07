84586b0eeae7a39c36200bef0a17709560583675
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2024-10-07 10:11:32 +0200

    Clean up `symbolic-base`

--- symbolic-base/examples/Examples/ReverseList.hs
+++
@@ -1,7 +0,0 @@
-module Examples.ReverseList (exampleReverseList) where
-
-import           ZkFold.Base.Data.Vector (Vector, reverse)
-
--- | Reverses the order of elements in a vector
-exampleReverseList :: Vector n t -> Vector n t
-exampleReverseList = reverse
