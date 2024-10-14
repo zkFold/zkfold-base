5276a1d9362398fb1130263fbe9d69033d8e7646
Eitan Chatav <eitan.chatav@gmail.com>
2024-10-14 12:57:40 -0700

    sanity check comment
    and needed aeson code

--- symbolic-base/src/ZkFold/Symbolic/Data/KYC.hs
+++ symbolic-base/src/ZkFold/Symbolic/Data/KYC.hs
@@ -14,9 +14,21 @@ import Data.Aeson
 import ZkFold.Symbolic.Interpreter (Interpreter)
 import ZkFold.Base.Algebra.Basic.Field (Zp)
 
-
 type KYCByteString context = ByteString 256 context
 
+{-
+>>> type Prime256_1 = 28948022309329048855892746252171976963363056481941560715954676764349967630337
+>>> :{
+exKYC :: KYCData (Interpreter (Zp Prime256_1))
+exKYC = KYCData
+  (fromConstant (1000 :: Natural))
+  (fromConstant (2000 :: Natural))
+  (fromConstant (3000 :: Natural))
+  (fromConstant (4000 :: Natural))
+:}
+>>> encode exKYC
+"{\"kycHash\":\"bb8\",\"kycID\":4000,\"kycType\":\"3e8\",\"kycValue\":\"7d0\"}"
+-}
 data KYCData context = KYCData
     { kycType  :: KYCByteString context
     , kycValue :: KYCByteString context
@@ -26,7 +38,6 @@ data KYCData context = KYCData
 
 instance (Symbolic context) => FromJSON (KYCData context)
 instance (Symbolic (Interpreter (Zp p))) => ToJSON (KYCData (Interpreter (Zp p)))
-
 
 isCitizen :: (Symbolic c) => KYCByteString c -> Vector n (KYCByteString c) -> Bool c
 isCitizen = elem
