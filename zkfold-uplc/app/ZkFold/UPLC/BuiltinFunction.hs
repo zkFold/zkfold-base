770dae2b28cd9a9933511b1f82a7df7b01e39d5e
TurtlePU <sokolov.p64@gmail.com>
2024-10-25 17:17:08 +0300

    + mono/poly split

--- zkfold-uplc/app/ZkFold/UPLC/BuiltinFunction.hs
+++ zkfold-uplc/app/ZkFold/UPLC/BuiltinFunction.hs
@@ -1,27 +1,35 @@
-{-# LANGUAGE DataKinds      #-}
-{-# LANGUAGE GADTs          #-}
-{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE DataKinds        #-}
+{-# LANGUAGE GADTs            #-}
+{-# LANGUAGE PolyKinds        #-}
 {-# LANGUAGE TypeApplications #-}
 
 module ZkFold.UPLC.BuiltinFunction where
 
 import           ZkFold.UPLC.BuiltinType (BuiltinType (..), Fin (..))
 
-data BuiltinFunction (s :: [BuiltinType n]) (t :: BuiltinType n) where
-  BFInteger :: BuiltinIntegerFunction s t -> BuiltinFunction s t
-  BFByteString :: BuiltinByteStringFunction s t -> BuiltinFunction s t
-  BFString :: BuiltinStringFunction s t -> BuiltinFunction s t
-  BFAlgorithm :: BuiltinAlgorithm s t -> BuiltinFunction s t
-  IfThenElse :: BuiltinFunction @1 '[BTBool, BTVar Z, BTVar Z] (BTVar Z)
-  ChooseUnit :: BuiltinFunction @1 '[BTUnit, BTVar Z] (BTVar Z)
-  Trace :: BuiltinFunction @1 '[BTString, BTVar Z] (BTVar Z)
-  BFPair :: BuiltinPairFunction s t -> BuiltinFunction s t
-  BFList :: BuiltinListFunction s t -> BuiltinFunction s t
-  BFData :: BuiltinDataFunction s t -> BuiltinFunction s t
-  BFCurve :: BuiltinBLSFunction s t -> BuiltinFunction s t -- ^ Batch 4
-  BFBitwise :: BuiltinBitwiseFunction s t -> BuiltinFunction s t -- ^ Batch 5
+data BuiltinFunction s t
+  = BFMono (BuiltinMonoFunction s t)
+  | BFPoly (BuiltinPolyFunction s t)
 
-data BuiltinIntegerFunction (s :: [BuiltinType n]) (t :: BuiltinType n) where
+data BuiltinMonoFunction (s :: [BuiltinType 0]) (t :: BuiltinType 0)
+  = BMFInteger (BuiltinIntegerFunction s t)
+  | BMFByteString (BuiltinByteStringFunction s t)
+  | BMFString (BuiltinStringFunction s t)
+  | BMFAlgorithm (BuiltinAlgorithm s t)
+  | BMFData (BuiltinDataFunction s t)
+  | BMFCurve (BuiltinBLSFunction s t) -- ^ Batch 4
+  | BMFBitwise (BuiltinBitwiseFunction s t) -- ^ Batch 5
+
+data BuiltinPolyFunction (s :: [BuiltinType n]) (t :: BuiltinType n) where
+  IfThenElse :: BuiltinPolyFunction @1 '[BTBool, BTVar Z, BTVar Z] (BTVar Z)
+  ChooseUnit :: BuiltinPolyFunction @1 '[BTUnit, BTVar Z] (BTVar Z)
+  Trace :: BuiltinPolyFunction @1 '[BTString, BTVar Z] (BTVar Z)
+  FstPair :: BuiltinPolyFunction @2 '[BTPair (BTVar Z) (BTVar (S Z))] (BTVar Z)
+  SndPair :: BuiltinPolyFunction @2 '[BTPair (BTVar Z) (BTVar (S Z))] (BTVar (S Z))
+  BPFList :: BuiltinListFunction s t -> BuiltinPolyFunction s t
+  ChooseData :: BuiltinPolyFunction @1 '[BTData, BTVar Z, BTVar Z, BTVar Z, BTVar Z, BTVar Z] (BTVar Z)
+
+data BuiltinIntegerFunction (s :: [BuiltinType 0]) (t :: BuiltinType 0) where
   AddInteger :: BuiltinIntegerFunction '[BTInteger, BTInteger] BTInteger
   SubtractInteger :: BuiltinIntegerFunction '[BTInteger, BTInteger] BTInteger
   MultiplyInteger :: BuiltinIntegerFunction '[BTInteger, BTInteger] BTInteger
@@ -35,7 +43,7 @@ data BuiltinIntegerFunction (s :: [BuiltinType n]) (t :: BuiltinType n) where
   IntegerToByteString :: BuiltinIntegerFunction '[BTBool, BTInteger, BTInteger] BTByteString -- ^ Batch 4
   ByteStringToInteger :: BuiltinIntegerFunction '[BTBool, BTByteString] BTInteger -- ^ Batch 4
 
-data BuiltinByteStringFunction (s :: [BuiltinType n]) (t :: BuiltinType n) where
+data BuiltinByteStringFunction (s :: [BuiltinType 0]) (t :: BuiltinType 0) where
   AppendByteString :: BuiltinByteStringFunction '[BTByteString, BTByteString] BTByteString
   ConsByteString :: BuiltinByteStringFunction '[BTInteger, BTByteString] BTByteString
   SliceByteString :: BuiltinByteStringFunction '[BTInteger, BTInteger, BTByteString] BTByteString
@@ -45,13 +53,13 @@ data BuiltinByteStringFunction (s :: [BuiltinType n]) (t :: BuiltinType n) where
   LessThanByteString :: BuiltinByteStringFunction '[BTByteString, BTByteString] BTBool
   LessThanEqualsByteString :: BuiltinByteStringFunction '[BTByteString, BTByteString] BTBool
 
-data BuiltinStringFunction (s :: [BuiltinType n]) (t :: BuiltinType n) where
+data BuiltinStringFunction (s :: [BuiltinType 0]) (t :: BuiltinType 0) where
   AppendString :: BuiltinStringFunction '[BTString, BTString] BTString
   EqualsString :: BuiltinStringFunction '[BTString, BTString] BTBool
   EncodeUtf8 :: BuiltinStringFunction '[BTString] BTByteString
   DecodeUtf8 :: BuiltinStringFunction '[BTByteString] BTString
 
-data BuiltinAlgorithm (s :: [BuiltinType n]) (t :: BuiltinType n) where
+data BuiltinAlgorithm (s :: [BuiltinType 0]) (t :: BuiltinType 0) where
   SHA2_256 :: BuiltinAlgorithm '[BTByteString] BTByteString
   SHA3_256 :: BuiltinAlgorithm '[BTByteString] BTByteString
   Blake2b_256 :: BuiltinAlgorithm '[BTByteString] BTByteString
@@ -62,10 +70,6 @@ data BuiltinAlgorithm (s :: [BuiltinType n]) (t :: BuiltinType n) where
   Keccak_256 :: BuiltinAlgorithm '[BTByteString] BTByteString -- ^ Batch 4
   Ripemd_160 :: BuiltinAlgorithm '[BTByteString] BTByteString -- ^ Batch 5
 
-data BuiltinPairFunction (s :: [BuiltinType n]) (t :: BuiltinType n) where
-  FstPair :: BuiltinPairFunction @2 '[BTPair (BTVar Z) (BTVar (S Z))] (BTVar Z)
-  SndPair :: BuiltinPairFunction @2 '[BTPair (BTVar Z) (BTVar (S Z))] (BTVar (S Z))
-
 data BuiltinListFunction (s :: [BuiltinType n]) (t :: BuiltinType n) where
   ChooseList :: BuiltinListFunction @2 '[BTList (BTVar (S Z)), BTVar Z, BTVar Z] (BTVar Z)
   MkCons :: BuiltinListFunction @1 '[BTVar Z, BTList (BTVar Z)] (BTList (BTVar Z))
@@ -73,8 +77,7 @@ data BuiltinListFunction (s :: [BuiltinType n]) (t :: BuiltinType n) where
   TailList :: BuiltinListFunction @1 '[BTList (BTVar Z)] (BTList (BTVar Z))
   NullList :: BuiltinListFunction @1 '[BTList (BTVar Z)] BTBool
 
-data BuiltinDataFunction (s :: [BuiltinType n]) (t :: BuiltinType n) where
-  ChooseData :: BuiltinDataFunction @1 '[BTData, BTVar Z, BTVar Z, BTVar Z, BTVar Z, BTVar Z] (BTVar Z)
+data BuiltinDataFunction (s :: [BuiltinType 0]) (t :: BuiltinType 0) where
   ConstrData :: BuiltinDataFunction '[BTInteger, BTList BTData] BTData
   MapData :: BuiltinDataFunction '[BTList (BTPair BTData BTData)] BTData
   ListData :: BuiltinDataFunction '[BTList BTData] BTData
@@ -91,14 +94,14 @@ data BuiltinDataFunction (s :: [BuiltinType n]) (t :: BuiltinType n) where
   MkNilPairData :: BuiltinDataFunction '[BTUnit] (BTList (BTPair BTData BTData))
   SerializeData :: BuiltinDataFunction '[BTData] BTByteString -- ^ Batch 2
 
-data BuiltinBLSFunction (s :: [BuiltinType n]) (t :: BuiltinType n) where
+data BuiltinBLSFunction (s :: [BuiltinType 0]) (t :: BuiltinType 0) where
   BLS_G1 :: BuiltinBLSG1Function s t -> BuiltinBLSFunction s t
   BLS_G2 :: BuiltinBLSG2Function s t -> BuiltinBLSFunction s t
   Bls12_381_millerLoop :: BuiltinBLSFunction '[BTBLSG1, BTBLSG2] BTBLSMLResult
   Bls12_381_mulMlResult :: BuiltinBLSFunction '[BTBLSMLResult, BTBLSMLResult] BTBLSMLResult
   Bls12_381_finalVerify :: BuiltinBLSFunction '[BTBLSMLResult, BTBLSMLResult] BTBool
 
-data BuiltinBLSG1Function (s :: [BuiltinType n]) (t :: BuiltinType n) where
+data BuiltinBLSG1Function (s :: [BuiltinType 0]) (t :: BuiltinType 0) where
   Bls12_381_G1_add :: BuiltinBLSG1Function '[BTBLSG1, BTBLSG1] BTBLSG1
   Bls12_381_G1_neg :: BuiltinBLSG1Function '[BTBLSG1] BTBLSG1
   Bls12_381_G1_scalarMul :: BuiltinBLSG1Function '[BTInteger, BTBLSG1] BTBLSG1
@@ -107,7 +110,7 @@ data BuiltinBLSG1Function (s :: [BuiltinType n]) (t :: BuiltinType n) where
   Bls12_381_G1_compress :: BuiltinBLSG1Function '[BTBLSG1] BTByteString
   Bls12_381_G1_uncompress :: BuiltinBLSG1Function '[BTByteString] BTBLSG1
 
-data BuiltinBLSG2Function (s :: [BuiltinType n]) (t :: BuiltinType n) where
+data BuiltinBLSG2Function (s :: [BuiltinType 0]) (t :: BuiltinType 0) where
   Bls12_381_G2_add :: BuiltinBLSG2Function '[BTBLSG2, BTBLSG2] BTBLSG2
   Bls12_381_G2_neg :: BuiltinBLSG2Function '[BTBLSG2] BTBLSG2
   Bls12_381_G2_scalarMul :: BuiltinBLSG2Function '[BTInteger, BTBLSG2] BTBLSG2
@@ -116,7 +119,7 @@ data BuiltinBLSG2Function (s :: [BuiltinType n]) (t :: BuiltinType n) where
   Bls12_381_G2_compress :: BuiltinBLSG2Function '[BTBLSG2] BTByteString
   Bls12_381_G2_uncompress :: BuiltinBLSG2Function '[BTByteString] BTBLSG2
 
-data BuiltinBitwiseFunction (s :: [BuiltinType n]) (t :: BuiltinType n) where
+data BuiltinBitwiseFunction (s :: [BuiltinType 0]) (t :: BuiltinType 0) where
   AndByteString :: BuiltinBitwiseFunction '[BTBool, BTByteString, BTByteString] BTByteString
   OrByteString :: BuiltinBitwiseFunction '[BTBool, BTByteString, BTByteString] BTByteString
   XorByteString :: BuiltinBitwiseFunction '[BTBool, BTByteString, BTByteString] BTByteString
