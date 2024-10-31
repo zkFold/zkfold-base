4c3771a7dd3c6a1d36ebc3eb15ffe66d65b2c3c1
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2024-10-31 15:22:29 +0100

    Add back the user-facing functions

--- symbolic-base/src/ZkFold/Base/Protocol/Protostar/Fold.hs
+++ symbolic-base/src/ZkFold/Base/Protocol/Protostar/Fold.hs
@@ -8,31 +8,26 @@ module ZkFold.Base.Protocol.Protostar.Fold where
 
 
 import           Control.DeepSeq                                  (NFData)
-import           Control.Lens                                     ((^.))
-import           Data.Binary                                      (Binary)
 import           Data.Function                                    ((.))
-import           Data.Functor                                     (fmap)
-import           Data.Kind                                        (Type)
 import           Data.Map.Strict                                  (Map)
 import qualified Data.Map.Strict                                  as M
 import           Data.Proxy                                       (Proxy)
-import           GHC.Generics                                     (Generic, Par1 (..), U1 (..), type (:*:) (..),
-                                                                   type (:.:) (..))
+import           GHC.Generics                                     (Generic, Par1)
 import           Prelude                                          (type (~), ($), (<$>), (<*>))
 import qualified Prelude                                          as P
 
 import           ZkFold.Base.Algebra.Basic.Class
 import           ZkFold.Base.Algebra.Basic.Number
 import qualified ZkFold.Base.Algebra.Polynomials.Univariate       as PU
-import           ZkFold.Base.Data.HFunctor                        (hmap)
 import           ZkFold.Base.Data.Vector                          (Vector)
-import           ZkFold.Base.Protocol.Protostar.Accumulator
+import           ZkFold.Base.Protocol.Protostar.Accumulator       hiding (pi)
 import qualified ZkFold.Base.Protocol.Protostar.AccumulatorScheme as Acc
 import           ZkFold.Base.Protocol.Protostar.ArithmeticCircuit ()
 import           ZkFold.Base.Protocol.Protostar.Commit
 import           ZkFold.Base.Protocol.Protostar.CommitOpen
 import           ZkFold.Base.Protocol.Protostar.FiatShamir
 import           ZkFold.Base.Protocol.Protostar.Oracle
+import           ZkFold.Base.Protocol.Protostar.SpecialSound      (SpecialSoundProtocol(..))
 import qualified ZkFold.Base.Protocol.Protostar.SpecialSound      as SPS
 import           ZkFold.Prelude                                   (replicate)
 import           ZkFold.Symbolic.Class
@@ -40,10 +35,7 @@ import           ZkFold.Symbolic.Compiler
 import           ZkFold.Symbolic.Data.Bool
 import           ZkFold.Symbolic.Data.Class
 import           ZkFold.Symbolic.Data.Eq
-import           ZkFold.Symbolic.Data.FieldElement                (FieldElement)
 import           ZkFold.Symbolic.Data.Input                       (SymbolicInput)
-import ZkFold.Base.Protocol.Protostar.SpecialSound (SpecialSoundProtocol(..))
-
 
 -- | These instances might seem off, but accumulator scheme requires this exact behaviour for ProverMessages which are Maps in this case.
 --
@@ -65,11 +57,6 @@ instance (Ring a, KnownNat n, KnownNat k) => Acc.LinearCombination (Vector n a) 
 instance (Ring a, KnownNat n) => Acc.LinearCombinationWith a (Vector n a) where
     linearCombinationWith coeff a b = (+) <$> P.fmap (coeff *) a <*> b
 
-
--- type C n a = ArithmeticCircuit a (Vector n) (Vector n)
--- type FS_CM ctx n comm a = FiatShamir (FieldElement ctx) (CommitOpen (SPS.MapMessage (FieldElement ctx) (C n a)) comm (C n a))
--- type Acc ctx n comm a = Accumulator (Vector n (FieldElement ctx)) (FieldElement ctx) comm (SPS.MapMessage (FieldElement ctx) (C n a))
-
 -- | The final result of recursion and the final accumulator.
 -- Accumulation decider is an arithmetizable function which can be called on the final accumulator.
 --
@@ -78,7 +65,7 @@ data ProtostarResult pi f c m
     { result           :: pi
     , finalAccumulator :: Accumulator pi f c m
     , finalAccProof    :: [c]
-    } deriving (Generic)
+    } deriving (GHC.Generics.Generic)
 
 deriving instance (NFData pi, NFData f, NFData c, NFData m) => NFData (ProtostarResult pi f c m)
 deriving instance (P.Show pi, P.Show f, P.Show c, P.Show m) => P.Show (ProtostarResult pi f c m)
@@ -86,7 +73,6 @@ deriving instance (P.Show pi, P.Show f, P.Show c, P.Show m) => P.Show (Protostar
 toFS
     :: forall pi f c m
     .  HomomorphicCommit f m c
-    => Input f (pi -> pi) ~ pi
     => f
     -> (pi -> pi)
     -> FiatShamir f (CommitOpen m c (pi -> pi))
@@ -115,63 +101,77 @@ ivcVerifier (i, pi_x, accTuple, acc'Tuple, pf) (a, ckTuple, dkTuple)
         dk = let ((x1, x2, x3, x4, x5), m) = dkTuple
               in Accumulator (AccumulatorInstance x1 [x2] [x3] x4 x5) [m]
 
--- -- TODO: this is insane
--- ivcVerifierAc
---     :: forall i f c m ctx a y t
---     .  Symbolic ctx
---     => SymbolicInput (i, c, (i, c, f, c, f), (i, c, f, c, f), c)
---     => SymbolicInput (a, (f, (f, f)), ((i, c, f, c, f), m))
---     => SymbolicData y
---     => Context a ~ ctx
---     => Context i ~ ctx
---     => Context y ~ ctx
---     => Support y ~ Proxy ctx
---     => Layout y ~ Par1
---     => t ~ ((i,c,(i,c,f,c,f),(i,c,f,c,f),c),(a,(f,f,f),(i,c,f,c,f),m),Proxy ctx)
---     => ctx ~ ArithmeticCircuit a (Layout t)
---     => Acc.AccumulatorScheme i f c m a
---     => y
--- ivcVerifierAc = compile (ivcVerifier @i @f @c @m @a)
+-- TODO: replace it with proper zero checks
+ivcVerifier0
+    :: forall pi f c m a ctx
+    .  AdditiveMonoid ((f, pi, [f], [c], c), ([c], c))
+    => Eq (Bool ctx) ((f, pi, [f], [c], c), ([c], c))
+    => Acc.AccumulatorScheme pi f c m a
+    => (pi, c, (pi, c, f, c, f), (pi, c, f, c, f), c)
+    -> (a, (f, (f, f)), ((pi, c, f, c, f), m))
+    -> Bool ctx
+ivcVerifier0 arg1 arg2 = ivcVerifier arg1 arg2 == zero
 
--- iterate
---     :: forall ctx n comm a
---     .  KnownNat n
---     => Arithmetic a
---     => Binary a
---     => Scale a (BaseField ctx)
---     => FromConstant a (BaseField ctx)
---     => Eq (Bool ctx) comm
---     => Eq (Bool ctx) [comm]
---     => Eq (Bool ctx) [FieldElement ctx]
---     => Eq (Bool ctx) (Vector n (FieldElement ctx))
---     => RandomOracle comm (FieldElement ctx)
---     => HomomorphicCommit (FieldElement ctx) [FieldElement ctx] comm
---     => HomomorphicCommit (FieldElement ctx) [SPS.MapMessage (FieldElement ctx) (C n a)] comm
---     => AdditiveGroup comm
---     => Scale a (PU.PolyVec (FieldElement ctx) 3)
---     => Scale a (FieldElement ctx)
---     => Scale (FieldElement ctx) comm
---     => ctx ~ ArithmeticCircuit a (Vector n)
---     => SPS.Input (FieldElement ctx) (C n a) ~ Vector n (FieldElement ctx)
---     => (forall c. (Symbolic c, BaseField c ~ a) => Vector n (FieldElement c) -> Vector n (FieldElement c))
---     -> Vector n a
---     -> Natural
---     -> ProtostarResult ctx n comm a
--- iterate f i0 n = iteration n ck f ac i0_arith i0 initialAccumulator (Acc.KeyScale one one)
---     where
---         i0_arith :: Vector n (FieldElement ctx)
---         i0_arith = fromConstant <$> i0
+-- TODO: this is insane
+ivcVerifierAc
+    :: forall pi f c m ctx a y t
+    .  AdditiveMonoid ((f, pi, [f], [c], c), ([c], c))
+    => Eq (Bool ctx) ((f, pi, [f], [c], c), ([c], c))
+    => Symbolic ctx
+    => SymbolicInput (pi, c, (pi, c, f, c, f), (pi, c, f, c, f), c)
+    => SymbolicInput (a, (f, (f, f)), ((pi, c, f, c, f), m))
+    => SymbolicData y
+    => Context a ~ ctx
+    => Context pi ~ ctx
+    => Context y ~ ctx
+    => Support y ~ Proxy ctx
+    => Layout y ~ GHC.Generics.Par1
+    => t ~ ((pi,c,(pi,c,f,c,f),(pi,c,f,c,f),c),(a,(f,f,f),(pi,c,f,c,f),m),Proxy ctx)
+    => ctx ~ ArithmeticCircuit a (Layout t)
+    => Acc.AccumulatorScheme pi f c m a
+    => y
+ivcVerifierAc = compile (ivcVerifier0 @pi @f @c @m @a)
 
---         ac :: C n a
---         ac = hmap (fmap unPar1 . unComp1) $ hlmap ((:*: U1) . Comp1 . fmap Par1) (compile @a f)
+iterate
+    :: forall pi f c m
+    .  AdditiveGroup pi
+    => AdditiveGroup c
+    => AdditiveMonoid m
+    => RandomOracle pi f
+    => RandomOracle c f
+    => Acc.LinearCombination m (SPS.MapMessage (PU.PolyVec f (Degree (pi -> pi) + 1)) (pi -> pi))
+    => Acc.LinearCombination pi (SPS.MapInput (PU.PolyVec f (Degree (pi -> pi) + 1)) (pi -> pi))
+    => Acc.LinearCombinationWith f pi
+    => SPS.AlgebraicMap f (pi -> pi)
+    => SPS.AlgebraicMap (PU.PolyVec f (Degree (pi -> pi) + 1)) (pi -> pi)
+    => HomomorphicCommit f [f] c
+    => HomomorphicCommit f m c
+    => SpecialSoundProtocol f (pi -> pi)
+    => KnownNat (Degree (pi -> pi) + 1)
+    => Witness f (pi -> pi) ~ ()
+    => Input f (pi -> pi) ~ pi
+    => ProverMessage f (pi -> pi) ~ m
+    => SPS.MapInput f (pi -> pi) ~ pi
+    => SPS.MapMessage f (pi -> pi) ~ m
+    => Scale f c
+    => Scale f m
+    => Ring f
+    =>  (pi -> pi)
+    -> pi
+    -> Natural
+    -> ProtostarResult pi f c m
+iterate func pi n = iteration n ck func res
+    where
+        initE = hcommit ck $ replicate (SPS.outputLength @f func) (zero :: f)
 
---         initE = hcommit ck $ replicate (SPS.outputLength @(FieldElement ctx) ac) (zero :: FieldElement ctx)
+        ck :: f
+        ck = oracle pi
 
---         ck :: FieldElement ctx
---         ck = oracle i0_arith
+        initialAccumulator :: Accumulator pi f c m
+        initialAccumulator = Accumulator (AccumulatorInstance zero (P.map (P.const zero) [1.. rounds @f func]) (P.map (P.const zero) [1 .. rounds @f func]) initE zero) [zero]
 
---         initialAccumulator :: Acc ctx n comm a
---         initialAccumulator = Accumulator (AccumulatorInstance (P.pure zero) [hcommit ck [zero :: SPS.MapMessage (FieldElement ctx) (C n a)]] [] initE zero) [zero]
+        res :: ProtostarResult pi f c m
+        res = ProtostarResult pi initialAccumulator []
 
 instanceProof
     :: forall pi f c m
