d0f828694d85ff0d128ed8f3b7801fada0ce69f3
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2024-10-31 16:00:23 +0100

    Fix `Gate.hs`

--- symbolic-base/src/ZkFold/Base/Protocol/Protostar/Gate.hs
+++ symbolic-base/src/ZkFold/Base/Protocol/Protostar/Gate.hs
@@ -13,7 +13,6 @@ import           ZkFold.Base.Data.Matrix                      (Matrix (..), oute
 import qualified ZkFold.Base.Data.Vector                      as V
 import           ZkFold.Base.Data.Vector                      (Vector)
 import           ZkFold.Base.Protocol.Protostar.SpecialSound  (AlgebraicMap (..), SpecialSoundProtocol (..))
-import           ZkFold.Symbolic.MonadCircuit                 (Arithmetic)
 
 newtype PolynomialProtostar f c d = PolynomialProtostar (Poly f (Zp c) Natural)
   deriving (Show, Eq, Ord)
@@ -28,7 +27,7 @@ deriving instance Polynomial f (Zp c) Natural => AdditiveGroup (PolynomialProtos
 data ProtostarGate (m :: Natural) (n :: Natural) (c :: Natural) (d :: Natural)
     deriving Generic
 
-instance (Arithmetic f, KnownNat m, KnownNat n) => SpecialSoundProtocol f (ProtostarGate m n c d) where
+instance (Ring f, KnownNat m, KnownNat n) => SpecialSoundProtocol f (ProtostarGate m n c d) where
     type Witness f (ProtostarGate m n c d)         = Vector n (Vector c f)
     -- ^ [(a_j, w_j)]_{j=1}^n where [w_j]_{j=1}^n is from the paper together and [a_j]_{j=1}^n are their absolute indices
     type Input f (ProtostarGate m n c d)           = (Matrix m n f, Vector m (PolynomialProtostar f c d))
@@ -36,7 +35,7 @@ instance (Arithmetic f, KnownNat m, KnownNat n) => SpecialSoundProtocol f (Proto
     type ProverMessage f (ProtostarGate m n c d)   = Vector n (Vector c f)
     -- ^ same as Witness
     type VerifierMessage f (ProtostarGate m n c d) = ()
-    type VerifierOutput f (ProtostarGate m n c d)  = Bool
+    type VerifierOutput f (ProtostarGate m n c d)  = [f]
 
     type Degree (ProtostarGate m n c d)            = d
 
@@ -57,11 +56,11 @@ instance (Arithmetic f, KnownNat m, KnownNat n) => SpecialSoundProtocol f (Proto
              -> Input f (ProtostarGate m n c d)
              -> [ProverMessage f (ProtostarGate m n c d)]
              -> [f]
-             -> Bool
-    verifier gate (s, g) [w] ts = all (== zero) $ algebraicMap gate (s, g) [w] ts one
+             -> [f]
+    verifier gate (s, g) [w] ts = algebraicMap @f gate (s, g) [w] ts one
     verifier _ _ _ _            = error "Invalid transcript"
 
-instance (Arithmetic f, KnownNat m, KnownNat n) => AlgebraicMap f (ProtostarGate m n c d) where
+instance (Ring f, KnownNat m, KnownNat n) => AlgebraicMap f (ProtostarGate m n c d) where
     type MapInput f (ProtostarGate m n c d)    = (Matrix m n f, Vector m (PolynomialProtostar f c d))
     type MapMessage f (ProtostarGate m n c d)  = Vector n (Vector c f)
 
