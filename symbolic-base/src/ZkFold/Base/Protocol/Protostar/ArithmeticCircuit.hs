f125792206cbb458b159077587e6dd2817121186
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2024-11-05 09:00:05 +0100

    Update ArithmeticCircuit.hs

--- symbolic-base/src/ZkFold/Base/Protocol/Protostar/ArithmeticCircuit.hs
+++ symbolic-base/src/ZkFold/Base/Protocol/Protostar/ArithmeticCircuit.hs
@@ -12,7 +12,7 @@ import           Data.Functor.Rep                                    (tabulate)
 import           Data.List                                           (foldl')
 import           Data.Map.Strict                                     (Map)
 import qualified Data.Map.Strict                                     as M
-import           Prelude                                             (fmap, ($), (.), (<$>))
+import           Prelude                                             (type (~), fmap, ($), (.), (<$>), undefined)
 import qualified Prelude                                             as P
 
 import           ZkFold.Base.Algebra.Basic.Class
@@ -26,25 +26,16 @@ import qualified ZkFold.Base.Protocol.Protostar.SpecialSound         as SPS
 import           ZkFold.Symbolic.Class
 import           ZkFold.Symbolic.Compiler
 import           ZkFold.Symbolic.Compiler.ArithmeticCircuit.Internal
-import           ZkFold.Symbolic.Data.Bool
 import           ZkFold.Symbolic.Data.Eq
 import           ZkFold.Symbolic.Data.FieldElement
 
-instance
-    ( Arithmetic a
-    , Symbolic ctx
-    , FromConstant a (BaseField ctx)
-    , Scale a (BaseField ctx)
-    ) => SPS.SpecialSoundProtocol (FieldElement ctx) (ArithmeticCircuit a (Vector n) o) where
+instance Arithmetic a => SPS.SpecialSoundProtocol a (ArithmeticCircuit a (Vector n) o) where
 
-    type Witness (FieldElement ctx) (ArithmeticCircuit a (Vector n) o) = (Vector n a, Map ByteString a)
-    -- ^ Witness does not contain input variables which may be present in the output.
-    -- Input vector and a map form the complete witness
-
-    type Input (FieldElement ctx) (ArithmeticCircuit a (Vector n) o) = Vector n (FieldElement ctx)
-    type ProverMessage (FieldElement ctx) (ArithmeticCircuit a (Vector n) o) = Map ByteString (FieldElement ctx)
-    type VerifierMessage (FieldElement ctx) (ArithmeticCircuit a (Vector n) o) = FieldElement ctx
-    type VerifierOutput (FieldElement ctx) (ArithmeticCircuit a (Vector n) o)  = Bool ctx
+    type Witness a (ArithmeticCircuit a (Vector n) o) = ()
+    type Input a (ArithmeticCircuit a (Vector n) o) = Vector n a
+    type ProverMessage a (ArithmeticCircuit a (Vector n) o) = Map ByteString a
+    type VerifierMessage a (ArithmeticCircuit a (Vector n) o) = a
+    type VerifierOutput a (ArithmeticCircuit a (Vector n) o)  = [a]
     type Degree (ArithmeticCircuit a (Vector n) o) = 2
 
     -- One round for Plonk
@@ -55,18 +46,44 @@ instance
     -- The transcript will be empty at this point, it is a one-round protocol.
     -- Input is arithmetised. We need to combine its witness with the circuit's witness.
     --
-    prover ac (inputVec, inputWitness) _ _ _ = fromConstant <$> M.union inputWitness (witnessGenerator ac inputVec)
+    prover ac _ pi _ _ = witnessGenerator ac pi
+
+    -- | Evaluate the algebraic map on public inputs and prover messages and compare it to a list of zeros
+    --
+    verifier rc i pm ts = SPS.algebraicMap rc i pm ts one
+
+instance
+    ( Arithmetic a
+    , Symbolic ctx
+    , BaseField ctx ~ a
+    ) => SPS.SpecialSoundProtocol (FieldElement ctx) (ArithmeticCircuit a (Vector n) o) where
+
+    type Witness (FieldElement ctx) (ArithmeticCircuit a (Vector n) o) = ()
+    type Input (FieldElement ctx) (ArithmeticCircuit a (Vector n) o) = Vector n (FieldElement ctx)
+    type ProverMessage (FieldElement ctx) (ArithmeticCircuit a (Vector n) o) = Map ByteString (FieldElement ctx)
+    type VerifierMessage (FieldElement ctx) (ArithmeticCircuit a (Vector n) o) = FieldElement ctx
+    type VerifierOutput (FieldElement ctx) (ArithmeticCircuit a (Vector n) o)  = [FieldElement ctx]
+    type Degree (ArithmeticCircuit a (Vector n) o) = 2
+
+    -- One round for Plonk
+    rounds = P.const 1
+
+    outputLength ac = P.fromIntegral $ M.size (acSystem ac)
+
+    -- The transcript will be empty at this point, it is a one-round protocol.
+    -- Input is arithmetised. We need to combine its witness with the circuit's witness.
+    --
+    prover = undefined
 
 
     -- | Evaluate the algebraic map on public inputs and prover messages and compare it to a list of zeros
     --
-    verifier rc i pm ts = all (== zero) $ SPS.algebraicMap @(FieldElement ctx) rc i pm ts one
+    verifier rc i pm ts = SPS.algebraicMap @(FieldElement ctx) rc i pm ts one
 
 instance
   ( Arithmetic a
   , Scale a f
   , MultiplicativeMonoid f
-  , Exponent f Natural
   , AdditiveMonoid f
   ) => SPS.AlgebraicMap f (ArithmeticCircuit a (Vector n) o) where
 
@@ -96,8 +113,7 @@ instance
 padDecomposition
     :: forall f d
     .  KnownNat d
-    => MultiplicativeSemigroup f
-    => Exponent f Natural
+    => MultiplicativeMonoid f
     => AdditiveMonoid f
     => f -> V.Vector d [f] -> [f]
 padDecomposition pad = foldl' (P.zipWith (+)) (P.repeat zero) . V.mapWithIx (\j p -> ((pad ^ (d -! j)) * ) <$> p)
