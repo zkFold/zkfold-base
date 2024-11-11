33c43499b8ecc01eb00e6fec6e2aff8c9b6d3a90
Eitan Chatav <eitan.chatav@gmail.com>
2024-11-11 15:48:34 -0800

    more Update fields

--- zkfold-ledger/src/ZkFold/Symbolic/Ledger/Types/Update.hs
+++ zkfold-ledger/src/ZkFold/Symbolic/Ledger/Types/Update.hs
@@ -6,8 +6,11 @@ import           ZkFold.Symbolic.Data.Bool              (Bool)
 import           ZkFold.Symbolic.Data.Combinators       (RegisterSize (Auto))
 import           ZkFold.Symbolic.Data.List              (List)
 import           ZkFold.Symbolic.Data.UInt              (UInt)
-import           ZkFold.Symbolic.Ledger.Types.Contract  (ContractData, ContractId)
+import           ZkFold.Symbolic.Ledger.Types.Address   (Address)
+import           ZkFold.Symbolic.Ledger.Types.Contract  (ContractData)
 import           ZkFold.Symbolic.Ledger.Types.Hash      (Hash)
+import           ZkFold.Symbolic.Ledger.Types.Input     (Input)
+import           ZkFold.Symbolic.Ledger.Types.Output    (Output)
 import           ZkFold.Symbolic.Ledger.Types.OutputRef (TransactionId)
 
 -- TODO: Add contract public data to the update.
@@ -18,16 +21,31 @@ type UpdateId = Hash
 type AddressIndex = UInt 40 Auto
 
 data Update context = Update
-  { updateNewAssignments  :: List context (ContractId context, AddressIndex context)
+  { updateNewAssignments  :: List context (Address context, AddressIndex context)
     -- ^ the map from addresses into assigned indices. Only new assignments.
   , updateSpentOutputs    :: List context (AddressIndex context, Bool context)
     -- ^ the map from address indices into boolean values.
     -- The keys are all indices of addresses from which an output was spent in the update.
-    -- The boolean values indicate whether the transactions that spent outputs from a particular address should be included in the Update.
+    -- The boolean values indicate whether the transactions
+    -- that spent outputs from a particular address should be included in the Update.
   , updateTransactions    :: List context (AddressIndex context, TransactionId context)
-    -- ^ the map from address indices into transaction id lists. Contains offline transactions.
+    -- ^ the map from address indices into transaction id lists.
+    -- Contains offline transactions.
   , updateTransactionData :: List context (AddressIndex context, ContractData context)
-    -- ^ the map from address indices into the accumulated public data items. Only non-empty public data.
+    -- ^ the map from address indices into the accumulated public data items.
+    -- Only non-empty public data.
+  , updateIndicesReleased :: List context (AddressIndex context)
+    -- ^ the list of indices that are being released after the update.
+  , updateBridgedOutputs  :: List context (Address context, List context (Output context))
+    -- ^ outputs that were bridged into the ledger.
+    -- Note that there are two ways to create transaction inputs in the ledger:
+    -- as transaction outputs of ledger transactions and as bridged outputs.
+    -- We will need to associate some `InputRef` with the latter, too.
+  , updateBridgedInputs   :: List context (AddressIndex context, List context (Input context))
+    -- ^ inputs that were bridged out of the ledger.
+    -- In order to bridge out of the ledger,
+    -- a user proves that there is a transaction in this update
+    -- that produced this Input.
   }
 
 updateId :: Update context -> UpdateId context
