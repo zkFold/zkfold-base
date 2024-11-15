9ac2f2c0cc606fff1bab16be6ce29b4f3dd221c8
hovanja2011 <hovanja2011@users.noreply.github.com>
2024-11-15 14:50:49 +0000

    stylish-haskell auto-commit

--- symbolic-base/src/ZkFold/Base/Protocol/Plonkup/PlonkConstraint.hs
+++ symbolic-base/src/ZkFold/Base/Protocol/Plonkup/PlonkConstraint.hs
@@ -87,7 +87,7 @@ toPlonkConstraint p =
 
     in case mapMaybe getCoefs perms of
         [] -> toPlonkConstraint zero
-        _ -> head $ mapMaybe getCoefs perms
+        _  -> head $ mapMaybe getCoefs perms
 
 fromPlonkConstraint :: (Ord a, Field a, KnownNat i) => PlonkConstraint i a -> Poly a (Var a (Vector i)) Natural
 fromPlonkConstraint (PlonkConstraint qm ql qr qo qc a b c) =
