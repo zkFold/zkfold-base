df941df33f78f4139118beea4cb97ad5dcce8bea
echatav <echatav@users.noreply.github.com>
2024-11-15 18:31:04 +0000

    stylish-haskell auto-commit

--- zkfold-ledger/src/ZkFold/Symbolic/Ledger/Validation/Input.hs
+++ zkfold-ledger/src/ZkFold/Symbolic/Ledger/Validation/Input.hs
@@ -1,11 +1,13 @@
 module ZkFold.Symbolic.Ledger.Validation.Input where
 
-import           Prelude                                  hiding (Bool, Eq, Maybe, all, any, filter, head, init, last,
-                                                           length, maybe, splitAt, tail, (&&), (*), (+), (/=), (==), (++), (!!), concat)
+import           Prelude                                  hiding (Bool, Eq, Maybe, all, any, concat, filter, head, init,
+                                                           last, length, maybe, splitAt, tail, (!!), (&&), (*), (+),
+                                                           (++), (/=), (==))
 
 import           ZkFold.Symbolic.Data.Bool                (Bool, all, any, false, (&&))
 import           ZkFold.Symbolic.Data.Eq                  (Eq (..))
-import           ZkFold.Symbolic.Data.List                (List, emptyList, filter, findList, head, last, (.:), (++), singleton, (!!), concat)
+import           ZkFold.Symbolic.Data.List                (List, concat, emptyList, filter, findList, head, last,
+                                                           singleton, (!!), (++), (.:))
 import           ZkFold.Symbolic.Data.Maybe               (maybe)
 import           ZkFold.Symbolic.Ledger.Types
 import           ZkFold.Symbolic.Ledger.Validation.Common (updateChainIsValid)
@@ -20,8 +22,8 @@ data InputWitness context = InputWitness
     }
 
 data InputWitnessDatum context = InputWitnessDatum
-    { inputWitnessUpdate :: Update context
-    , inputWitnessOnlineTxs :: List context (AddressIndex context, Transaction context)
+    { inputWitnessUpdate     :: Update context
+    , inputWitnessOnlineTxs  :: List context (AddressIndex context, Transaction context)
     , inputWitnessOfflineTxs :: List context (Transaction context)
     }
 
