416bcd9b25dabf6ee9c276fc36f932899698b48f
Eitan Chatav <eitan.chatav@gmail.com>
2024-11-21 13:34:07 -0800

    Update Input.hs

--- symbolic-base/src/ZkFold/Symbolic/Data/Input.hs
+++ symbolic-base/src/ZkFold/Symbolic/Data/Input.hs
@@ -6,13 +6,11 @@ module ZkFold.Symbolic.Data.Input (
 ) where
 
 import           Control.DeepSeq                    (NFData)
-import           Data.Functor.Rep                   (Representable)
-import qualified Data.Functor.Rep              as R (Rep)
+import qualified Data.Functor.Rep              as R
 import           Data.Ord                           (Ord)
 import           Data.Type.Equality                 (type (~))
 import           Data.Typeable                      (Proxy (..))
-import           GHC.Generics                       (Par1 (..), (:*:) (..), Generic)
-import qualified GHC.Generics                  as G (Rep, from)
+import qualified GHC.Generics                  as G
 import           GHC.TypeLits                       (KnownNat)
 import           Prelude                            (foldl, ($), (.))
 
@@ -31,23 +29,23 @@ import           ZkFold.Symbolic.MonadCircuit
 class
     ( SymbolicData d
     , Support d ~ Proxy (Context d)
-    , Representable (Layout d)
+    , R.Representable (Layout d)
     , Binary (R.Rep (Layout d))
     , Ord (R.Rep (Layout d))
     , NFData (R.Rep (Layout d))
     ) => SymbolicInput d where
     isValid :: d -> Bool (Context d)
     default isValid ::
-      (Generic d, GSymbolicInput (G.Rep d), GContext (G.Rep d) ~ Context d)
+      (G.Generic d, GSymbolicInput (G.Rep d), GContext (G.Rep d) ~ Context d)
       => d -> Bool (Context d)
     isValid = gisValid @(G.Rep d) . G.from
 
 
 instance Symbolic c => SymbolicInput (Bool c) where
   isValid (Bool b) = Bool $ fromCircuitF b $
-      \(Par1 v) -> do
+      \(G.Par1 v) -> do
         u <- newAssigned (\x -> x v * (one - x v))
-        isZero $ Par1 u
+        isZero $ G.Par1 u
 
 
 instance
@@ -55,7 +53,7 @@ instance
   , Binary (R.Rep f)
   , Ord (R.Rep f)
   , NFData (R.Rep f)
-  , Representable f) => SymbolicInput (c f) where
+  , R.Representable f) => SymbolicInput (c f) where
   isValid _ = true
 
 
@@ -68,7 +66,6 @@ instance (
     , SymbolicInput x
     , SymbolicInput y
     ) => SymbolicInput (x, y) where
-  isValid (l, r) = isValid l && isValid r
 
 instance (
     Symbolic (Context x)
@@ -78,7 +75,6 @@ instance (
     , SymbolicInput y
     , SymbolicInput z
     ) => SymbolicInput (x, y, z) where
-  isValid (l, m, r) = isValid l && isValid m && isValid r
 
 instance (
   Symbolic (Context x)
@@ -99,6 +95,11 @@ instance
     , Symbolic (GContext u)
     , GSymbolicInput u
     , GSymbolicInput v
-    ) => GSymbolicInput (u :*: v) where
+    ) => GSymbolicInput (u G.:*: v) where
+    gisValid (u G.:*: v) = gisValid u && gisValid v
 
-    gisValid (u :*: v) = gisValid u && gisValid v
+instance GSymbolicInput u => GSymbolicInput (G.M1 i c u) where
+    gisValid (G.M1 u) = gisValid u
+
+instance SymbolicInput x => GSymbolicInput (G.Rec0 x) where
+    gisValid (G.K1 x) = isValid x
