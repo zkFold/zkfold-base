23ef15b026d21a54bef2eef853063fff21b01fdc
vlasin <vlasin@users.noreply.github.com>
2024-11-25 13:55:30 +0000

    stylish-haskell auto-commit

--- symbolic-base/src/ZkFold/Base/Protocol/Protostar/RecursiveCircuit.hs
+++ symbolic-base/src/ZkFold/Base/Protocol/Protostar/RecursiveCircuit.hs
@@ -4,13 +4,14 @@
 module ZkFold.Base.Protocol.Protostar.RecursiveCircuit where
 
 import           GHC.Generics                                          (Par1 (..), U1 (..), type (:.:) (..), (:*:) (..))
-import           Prelude                                               hiding (Num (..), drop, head, take, zipWith, replicate)
+import           Prelude                                               hiding (Num (..), drop, head, replicate, take,
+                                                                        zipWith)
 
 import           ZkFold.Base.Algebra.Basic.Class
 import           ZkFold.Base.Algebra.Basic.Number                      (KnownNat, type (+), type (-), value)
 import           ZkFold.Base.Data.ByteString                           (Binary)
 import           ZkFold.Base.Data.Vector                               (Vector, unsafeToVector)
-import           ZkFold.Base.Protocol.Protostar.ArithmetizableFunction (ArithmetizableFunction(ArithmetizableFunction))
+import           ZkFold.Base.Protocol.Protostar.ArithmetizableFunction (ArithmetizableFunction (ArithmetizableFunction))
 import           ZkFold.Base.Protocol.Protostar.Commit                 (HomomorphicCommit)
 import           ZkFold.Base.Protocol.Protostar.CommitOpen             (CommitOpen (..))
 import           ZkFold.Base.Protocol.Protostar.FiatShamir             (FiatShamir (..))
@@ -21,7 +22,7 @@ import           ZkFold.Symbolic.Compiler
 import           ZkFold.Symbolic.Data.Class                            (SymbolicData (..))
 import           ZkFold.Symbolic.Data.FieldElement                     (FieldElement (..))
 import           ZkFold.Symbolic.Data.Input                            (SymbolicInput)
-import           ZkFold.Symbolic.Interpreter                           (Interpreter(..))
+import           ZkFold.Symbolic.Interpreter                           (Interpreter (..))
 
 -- | Takes a function `f` and returns a circuit `C` with input `y` and witness `w`.
 -- The circuit is such that `C(y, w) = 0` implies that `y = x(n)` for some positive `n` where
