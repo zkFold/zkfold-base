1b12931bd3af83225884c2890f5ea00a3518e22a
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2024-11-29 14:23:41 +0100

    Update Commit.hs

--- symbolic-base/src/ZkFold/Base/Protocol/IVC/Commit.hs
+++ symbolic-base/src/ZkFold/Base/Protocol/IVC/Commit.hs
@@ -1,27 +1,19 @@
 {-# LANGUAGE AllowAmbiguousTypes  #-}
+{-# LANGUAGE TypeOperators        #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 module ZkFold.Base.Protocol.IVC.Commit (Commit (..), HomomorphicCommit (..), PedersonSetup (..)) where
 
-import           Control.DeepSeq                             (NFData)
-import           Data.Void                                   (Void)
-import           Data.Zip                                    (zipWith)
-import           GHC.IsList                                  (IsList (..))
+import           Data.Zip                                    (Zip (..))
 import           Prelude                                     hiding (Num (..), sum, take, zipWith)
 import           System.Random                               (Random (..), mkStdGen)
 
 import           ZkFold.Base.Algebra.Basic.Class
-import           ZkFold.Base.Algebra.Basic.Field             (Zp)
 import           ZkFold.Base.Algebra.Basic.Number
-import           ZkFold.Base.Algebra.EllipticCurve.BLS12_381
 import           ZkFold.Base.Algebra.EllipticCurve.Class
-import           ZkFold.Base.Algebra.EllipticCurve.Ed25519
-import           ZkFold.Base.Data.Vector                     (Vector)
+import           ZkFold.Base.Data.Vector                     (Vector, unsafeToVector)
 import           ZkFold.Base.Protocol.IVC.Oracle
 import           ZkFold.Prelude                              (take)
-import           ZkFold.Symbolic.Class                       (Symbolic)
-import           ZkFold.Symbolic.Data.Ed25519                ()
-import           ZkFold.Symbolic.Data.FFA                    (Size)
 
 -- | Commit to the object @a@ with commitment key @ck@ and results of type @f@
 --
@@ -36,24 +28,21 @@ instance RandomOracle algo a x => Commit algo a x where
 class AdditiveGroup c => HomomorphicCommit a c where
     hcommit :: a -> c
 
-class PedersonSetup n c where
-    groupElements :: Vector n c
+class PedersonSetup s c where
+    groupElements :: s c
 
-instance KnownNat n => PedersonSetup n (Point BLS12_381_G1) where
+type PedersonSetupMaxSize = 100
+
+instance (EllipticCurve curve, Random (ScalarField curve)) => PedersonSetup [] (Point curve) where
     groupElements =
         -- TODO: This is just for testing purposes! Not to be used in production
-        let x = fst $ random $ mkStdGen 0 :: Zp BLS12_381_Scalar
-        in fromList $ take (value @n) $ iterate (scale x) gen
+        let x = fst $ random $ mkStdGen 0 :: ScalarField curve
+        in take (value @PedersonSetupMaxSize) $ iterate (mul x) gen
 
-instance (KnownNat n, Symbolic c, NFData (c (Vector Size)))
-        => PedersonSetup n (Point (Ed25519 c)) where
+instance (KnownNat n, EllipticCurve curve, Random (ScalarField curve), n <= PedersonSetupMaxSize) => PedersonSetup (Vector n) (Point curve) where
     groupElements =
         -- TODO: This is just for testing purposes! Not to be used in production
-        let x = fst $ random $ mkStdGen 0 :: ScalarField (Ed25519 Void)
-        in fromList $ take (value @n) $ iterate (scale x) gen
+        unsafeToVector $ take (value @n) $ groupElements @[]
 
-instance (PedersonSetup n c, Scale f c, AdditiveGroup c) => HomomorphicCommit (Vector n f) c where
+instance (PedersonSetup s c, Zip s, Foldable s, Scale f c, AdditiveGroup c) => HomomorphicCommit (s f) c where
     hcommit v = sum $ zipWith scale v groupElements
-
-instance (PedersonSetup 100 c, Scale f c, AdditiveGroup c) => HomomorphicCommit [f] c where
-    hcommit v = sum $ zipWith scale v (toList $ groupElements @100)
