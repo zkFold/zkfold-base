edebc879412e401ca9f75a957d26e00f4672c8dd
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2024-11-30 09:36:31 +0100

    Update RecursionCircuit.hs

--- symbolic-base/src/ZkFold/Base/Protocol/IVC/RecursionCircuit.hs
+++ symbolic-base/src/ZkFold/Base/Protocol/IVC/RecursionCircuit.hs
@@ -1,13 +1,13 @@
 module ZkFold.Base.Protocol.IVC.RecursionCircuit where
 
-import           Prelude                                         hiding (Num (..), drop, head, replicate, take, zipWith)
+import           Prelude                            hiding (Num (..), drop, head, replicate, take, zipWith)
 
-import           ZkFold.Base.Data.Vector                         (Vector)
-import           ZkFold.Symbolic.Class
+import           ZkFold.Base.Protocol.IVC.Predicate (StepFunction)
 import           ZkFold.Symbolic.Compiler
-import           ZkFold.Symbolic.Data.FieldElement               (FieldElement (..))
 
 -- | Takes a function `f` and returns a Protostar recursion circuit `R`.
-ivcRecursionCircuit :: forall a payload input output nx nu . (forall ctx' . Symbolic ctx' => Vector nx (FieldElement ctx') -> Vector nu (FieldElement ctx') -> Vector nx (FieldElement ctx'))
-    -> ArithmeticCircuit a payload input output
-ivcRecursionCircuit _ = undefined
+ivcRecursionCircuit :: forall a payload input output nx nu .
+    StepFunction nx nu -> ArithmeticCircuit a payload input output
+ivcRecursionCircuit _ =
+    let
+    in undefined
