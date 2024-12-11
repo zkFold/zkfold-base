415faf00cea2105cec7dfc427570ace7b65c5d9a
echatav <echatav@users.noreply.github.com>
2024-12-11 01:32:50 +0000

    stylish-haskell auto-commit

--- zkfold-uplc/src/ZkFold/Symbolic/UPLC/Data.hs
+++ zkfold-uplc/src/ZkFold/Symbolic/UPLC/Data.hs
@@ -7,13 +7,13 @@
 
 module ZkFold.Symbolic.UPLC.Data where
 
-import           GHC.Generics               (Par1)
+import           GHC.Generics                     (Par1)
 
-import           ZkFold.Symbolic.Class      (Symbolic)
-import           ZkFold.Symbolic.Data.Bool (Bool)
-import           ZkFold.Symbolic.Data.Class (SymbolicData (..))
+import           ZkFold.Symbolic.Class            (Symbolic)
+import           ZkFold.Symbolic.Data.Bool        (Bool)
+import           ZkFold.Symbolic.Data.Class       (SymbolicData (..))
 import           ZkFold.Symbolic.Data.Conditional (Conditional)
-import           ZkFold.Symbolic.Data.Input (SymbolicInput)
+import           ZkFold.Symbolic.Data.Input       (SymbolicInput)
 
 -- | Plutus Core's Data as a Symbolic datatype.
 -- TODO: Proper symbolic Data type
