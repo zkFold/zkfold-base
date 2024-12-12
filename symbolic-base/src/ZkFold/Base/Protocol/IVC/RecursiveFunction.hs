2e6ed8b950dcbfac12d274c4d05b14434ab96716
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2024-12-12 16:51:52 +0100

    Update RecursiveFunction.hs

--- symbolic-base/src/ZkFold/Base/Protocol/IVC/RecursiveFunction.hs
+++ symbolic-base/src/ZkFold/Base/Protocol/IVC/RecursiveFunction.hs
@@ -14,6 +14,7 @@ import           ZkFold.Base.Data.Vector               (Vector)
 import           ZkFold.Base.Protocol.IVC.Accumulator  hiding (pi)
 import           ZkFold.Base.Protocol.IVC.Oracle
 import           ZkFold.Base.Protocol.IVC.Predicate    (StepFunction)
+import GHC.Base (undefined)
 
 -- | Public input to the recursive function
 data RecursiveI k i c f = RecursiveI (i f) (AccumulatorInstance k i c f)
@@ -22,9 +23,19 @@ data RecursiveI k i c f = RecursiveI (i f) (AccumulatorInstance k i c f)
 deriving instance (HashAlgorithm algo f, RandomOracle algo (i f) f, RandomOracle algo (c f) f) => RandomOracle algo (RecursiveI k i c f) f
 
 -- | Payload to the recursive function
-data RecursiveP d k i p c f = RecursiveP (i f) (p f) f (AccumulatorInstance k i c f) (Vector k (c f)) (Vector (d-1) (c f))
+data RecursiveP d k i p c f = RecursiveP (p f) f (Vector k (c f)) (Vector (d-1) (c f))
     deriving (GHC.Generics.Generic)
 
 -- TODO: Implement the recursive function.
-recursiveFunction :: StepFunction a i p -> StepFunction a i p
-recursiveFunction f = f
+recursiveFunction :: forall d k a i p c . StepFunction a i p -> StepFunction a (RecursiveI k i c) (RecursiveP d k i p c)
+recursiveFunction func =
+    let
+        funcRecursive :: StepFunction a (RecursiveI k i c) (RecursiveP d k i p c)
+        funcRecursive (RecursiveI x accX) (RecursiveP u flag piX pf) =
+            let
+                x' = func x u
+            in
+                -- RecursiveP i' p f acc' cs e
+                undefined
+
+    in funcRecursive
