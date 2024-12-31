606695fa8f549d95f289f3ac7d80f40bc0ad128f
Eitan Chatav <eitan.chatav@gmail.com>
2024-12-30 17:06:40 -0800

    more point types ...
    and ProjectivePlanar class

--- symbolic-base/src/ZkFold/Base/Algebra/EllipticCurve/Class2.hs
+++ symbolic-base/src/ZkFold/Base/Algebra/EllipticCurve/Class2.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE RebindableSyntax #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -Wno-redundant-constraints #-}
@@ -11,9 +12,15 @@ module ZkFold.Base.Algebra.EllipticCurve.Class2
     -- * point classes
   , Planar (..)
   , HasPointInf (..)
+  , ProjectivePlanar (..)
     -- * point types
-  , Point (..)
   , Weierstrass (..)
+  , Point (..)
+  , CompressedPoint (..)
+  , AffinePoint (..)
+  , CompressedAffinePoint (..)
+  , TwistedEdwardsPoint (..)
+  , Slope (..)
   ) where
 
 import GHC.Generics
@@ -25,7 +32,7 @@ import ZkFold.Symbolic.Data.Bool
 import ZkFold.Symbolic.Data.Conditional
 import ZkFold.Symbolic.Data.Eq
 
-{- | Elliptic curves are algebraic curves that form Abelian groups.
+{- | Elliptic curves are plane algebraic curves that form Abelian groups.
 Elliptic curves always have genus @1@ and are birationally equivalent
 to a curve of degree @3@. As such, elliptic curves are
 the least complicated curves after conic sections, curves of
@@ -43,7 +50,7 @@ class
   , Planar point
   , AdditiveGroup (point field)
   ) => EllipticCurve curve bool field point where
-    -- | `isOnCurve` validates an equation for an algebraic curve
+    -- | `isOnCurve` validates an equation for an plane algebraic curve
     -- which has degree 3 up to a birational equivalence.
     isOnCurve :: point field -> bool
 
@@ -65,44 +72,35 @@ class Planar point where pointXY :: field -> field -> point field
 -}
 class HasPointInf point where pointInf :: point
 
-{- | A type of points in the projective plane.
-
-* When `_zBit` is `false`, then `_x` and `_y` are coordinates of a finite point.
-* When `_zBit` is `true`, then `_y` `//` `_x` is
-the coordinate of a point on the projective line at infinity.
+{- | A class with a destructor method
+`casePoint` for handling finite and infinite points.
 -}
-data Point bool field = Point
-  { _x :: field
-  , _y :: field
-  , _zBit :: bool
-  } deriving Generic
-instance BoolType bool => Planar (Point bool) where
-  pointXY x y = Point x y false
-instance
-  ( BoolType bool
-  , Semiring field
-  ) => HasPointInf (Point bool field) where
-  pointInf = Point zero one true
-instance
-  ( BoolType bool
-  , Conditional bool bool
-  , Conditional bool field
-  ) => Conditional bool (Point bool field)
-instance
-  ( BoolType bool
-  , Conditional bool bool
-  , Eq bool bool
-  , Eq bool field
-  , Field field
-  ) => Eq bool (Point bool field) where
-    Point x0 y0 isInf0 == Point x1 y1 isInf1 =
-      if not isInf0 && not isInf1 then x0 == x1 && y0 == y1
-      else if isInf0 && isInf1
-      then x0 == zero && x1 == zero || x1*y0 == x0*y1 -- same slope y0//x0 = y1//x1
-      else false
-    pt0 /= pt1 = not (pt0 == pt1)
+class
+  ( Planar point
+  , HasPointInf (point field)
+  , BoolType bool
+  , FromConstant (AffinePoint field) (point field)
+  , FromConstant (Slope field) (point field)
+  ) => ProjectivePlanar bool field point where
 
-{- | `Weierstrass` newtype specifies a point on a particular `WeierstrassCurve`. -}
+
+    {- | `casePoint` expresses points as a disjoint union of
+
+          * finite `Planar` `AffinePoint`s;
+          * the projective line of `Slope`s at infinity.
+
+          Up to `Eq`uality of points,
+          embedding finite and infinite points with `fromConstant`,
+
+          prop> (id :: point field -> point field) = casePoint fromConstant fromConstant
+    -}
+    casePoint
+      :: (Conditional bool r, Eq bool r)
+      => (AffinePoint field -> r) -- ^ finite case
+      -> (Slope field -> r) -- ^ infinite case
+      -> point field -> r
+
+{- | The `Weierstrass` newtype specifies a point on a particular `WeierstrassCurve`. -}
 newtype Weierstrass curve point field = Weierstrass {pointWeierstrass :: point field}
 deriving newtype instance Conditional bool (point field)
   => Conditional bool (Weierstrass curve point field)
@@ -171,3 +169,87 @@ instance
   , Field field
   ) => Scale Integer (Weierstrass curve (Point bool) field) where
   scale = intScale
+
+{- | A type of points in the projective plane.
+
+* When `_zBit` is `false`, then `_x` and `_y` are coordinates of a finite point.
+* When `_zBit` is `true`, then `_y` `//` `_x` is
+the coordinate of a point on the projective line at infinity.
+-}
+data Point bool field = Point
+  { _x :: field
+  , _y :: field
+  , _zBit :: bool
+  } deriving Generic
+instance BoolType bool => Planar (Point bool) where
+  pointXY x y = Point x y false
+instance BoolType bool
+  => FromConstant (Slope field) (Point bool field) where
+    fromConstant (Slope x y) = Point x y true
+instance BoolType bool
+  => FromConstant (AffinePoint field) (Point bool field) where
+    fromConstant (AffinePoint x y) = pointXY x y
+instance
+  ( BoolType bool
+  , Semiring field
+  ) => HasPointInf (Point bool field) where
+  pointInf = Point zero one true
+instance
+  ( BoolType bool
+  , Semiring field
+  ) => ProjectivePlanar bool field (Point bool) where
+    casePoint fin infin (Point x y isInf) =
+      if isInf then infin (Slope x y) else fin (pointXY x y) 
+instance
+  ( BoolType bool
+  , Conditional bool bool
+  , Conditional bool field
+  ) => Conditional bool (Point bool field)
+instance
+  ( BoolType bool
+  , Conditional bool bool
+  , Eq bool bool
+  , Eq bool field
+  , Field field
+  ) => Eq bool (Point bool field) where
+    Point x0 y0 isInf0 == Point x1 y1 isInf1 =
+      if not isInf0 && not isInf1 then x0 == x1 && y0 == y1
+      else if isInf0 && isInf1
+      then x0 == zero && x1 == zero || x1*y0 == x0*y1 -- same slope y0//x0 = y1//x1
+      else false
+    pt0 /= pt1 = not (pt0 == pt1)
+
+data CompressedPoint bool field = CompressedPoint
+  { _x :: field
+  , _yBit :: bool
+  , _zBit :: bool
+  } deriving Generic
+
+data AffinePoint field = AffinePoint
+  { _x :: field
+  , _y :: field
+  } deriving Generic
+instance Planar AffinePoint where pointXY = AffinePoint
+
+data CompressedAffinePoint bool field = CompressedAffinePoint
+  { _xBit :: bool
+  , _y :: field
+  } deriving Generic
+
+data HomogeneousPoint field = HomogeneousPoint
+  { _x :: field
+  , _y :: field
+  , _z :: field
+  } deriving Generic
+
+data TwistedEdwardsPoint field = TwistedEdwardsPoint
+  { _x :: field
+  , _y :: field
+  , _t :: field
+  , _z :: field
+  } deriving Generic
+
+data Slope field = Slope
+  { _x :: field
+  , _y :: field
+  } deriving Generic
