c338387af529c13304d11381dc2fdcf71a38cef4
Eitan Chatav <eitan.chatav@gmail.com>
2025-01-06 11:37:03 -0800

    simplify pasta

--- symbolic-base/src/ZkFold/Base/Algebra/EllipticCurve2/Pasta.hs
+++ symbolic-base/src/ZkFold/Base/Algebra/EllipticCurve2/Pasta.hs
@@ -3,7 +3,8 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 module ZkFold.Base.Algebra.EllipticCurve2.Pasta
-  ( Pallas_Point
+  ( Pasta_Point
+  , Pallas_Point
   , Vesta_Point
   , FpModulus
   , FqModulus
@@ -12,7 +13,6 @@ module ZkFold.Base.Algebra.EllipticCurve2.Pasta
   ) where
 
 import           Control.Monad
-import           Data.Type.Equality
 import           Prelude                                  (($))
 import qualified Prelude
 
@@ -22,7 +22,6 @@ import           ZkFold.Base.Algebra.Basic.Number
 import           ZkFold.Base.Algebra.EllipticCurve2.Class
 import           ZkFold.Base.Data.ByteString
 import           ZkFold.Symbolic.Data.Bool
-import           ZkFold.Symbolic.Data.Conditional
 import           ZkFold.Symbolic.Data.Eq
 
 -------------------------------- Introducing Fields ----------------------------------
@@ -36,80 +35,52 @@ instance Prime FqModulus
 type Fp = Zp FpModulus
 type Fq = Zp FqModulus
 
------------------------------------- Pallas ------------------------------------
+------------------------------------ Pasta -------------------------------------
 
-type Pallas_Point = Weierstrass "Pallas" (Point Prelude.Bool) Fp
-
-instance Field field => WeierstrassCurve "Pallas" field where
+instance Field field => WeierstrassCurve "Pasta" field where
   weierstrassB = fromConstant (5 :: Natural)
 
-instance
-  ( Conditional bool bool
-  , Conditional bool baseField
-  , Eq bool baseField
-  , FiniteField baseField
-  , Order baseField ~ FpModulus
-  , scalarField ~ Fq
-  ) => SubgroupCurve "Pallas" bool baseField scalarField (Weierstrass "Pallas" (Point bool)) where
-    pointGen = pointXY
-      (fromConstant (0x40000000000000000000000000000000224698fc094cf91b992d30ed00000000 :: Natural))
-      (fromConstant (0x02 :: Natural))
+type Pasta_Point = Weierstrass "Pasta" (Point Prelude.Bool)
 
-instance
-  ( Conditional bool bool
-  , Conditional bool field
-  , Eq bool field
-  , Field field
-  ) => Scale Fq (Weierstrass "Pallas" (Point bool) field) where
+------------------------------------ Pallas ------------------------------------
+
+type Pallas_Point = Pasta_Point Fp
+
+instance SubgroupCurve "Pasta" Prelude.Bool Fp Fq Pasta_Point where
+  pointGen = pointXY
+    0x40000000000000000000000000000000224698fc094cf91b992d30ed00000000
+    0x02
+
+instance Scale Fq Pallas_Point where
     scale n x = scale (toConstant n) x
 
 ------------------------------------ Vesta ------------------------------------
 
-type Vesta_Point = Weierstrass "Vesta" (Point Prelude.Bool) Fq
+type Vesta_Point = Pasta_Point Fq
 
-instance Field field => WeierstrassCurve "Vesta" field where
-  weierstrassB = fromConstant (5 :: Natural)
+instance SubgroupCurve "Pasta" Prelude.Bool Fq Fp Pasta_Point where
+  pointGen = pointXY
+    0x40000000000000000000000000000000224698fc0994a8dd8c46eb2100000000
+    0x02
 
-instance
-  ( Conditional bool bool
-  , Conditional bool baseField
-  , Eq bool baseField
-  , FiniteField baseField
-  , Order baseField ~ FqModulus
-  , scalarField ~ Fp
-  ) => SubgroupCurve "Vesta" bool baseField scalarField (Weierstrass "Vesta" (Point bool)) where
-    pointGen = pointXY
-      (fromConstant (0x40000000000000000000000000000000224698fc0994a8dd8c46eb2100000000 :: Natural))
-      (fromConstant (0x02 :: Natural))
-
-instance
-  ( Conditional bool bool
-  , Conditional bool field
-  , Eq bool field
-  , Field field
-  ) => Scale Fp (Weierstrass "Vesta" (Point bool) field) where
+instance Scale Fp Vesta_Point where
     scale n x = scale (toConstant n) x
 
 ------------------------------------ Encoding ------------------------------------
 
-instance Binary Pallas_Point where
-  put (Weierstrass (Point xp yp isInf)) =
-    if isInf then put @Pallas_Point (pointXY zero zero) else put xp >> put yp
-  get = do
-    xp <- get
-    yp <- get
-    return $
-      if xp == zero && yp == zero
-      then pointInf
-      else pointXY xp yp
-
-instance Binary Vesta_Point where
-  put (Weierstrass (Point xp yp isInf)) =
-    if isInf then put @Vesta_Point (pointXY zero zero) else put xp >> put yp
-  get = do
-    xp <- get
-    yp <- get
-    return $
-      if xp == zero && yp == zero
-      then pointInf
-      else pointXY xp yp
+instance
+  ( Binary field
+  , Field field
+  , Eq Prelude.Bool field
+  ) => Binary (Pasta_Point field) where
+    put (Weierstrass (Point xp yp isInf)) =
+      if isInf
+      then put @(Pasta_Point field) (pointXY zero zero)
+      else put xp >> put yp
+    get = do
+      xp <- get
+      yp <- get
+      return $
+        if xp == zero && yp == zero
+        then pointInf
+        else pointXY xp yp
