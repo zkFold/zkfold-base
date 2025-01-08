0712c197625b58a198d22c7113a79680bb098eba
TurtlePU <sokolov.p64@gmail.com>
2025-01-08 14:20:41 +0300

    + separate GoldenTest benchmark

--- symbolic-examples/bench/BenchCompiler.hs
+++ symbolic-examples/bench/BenchCompiler.hs
@@ -2,27 +2,16 @@ module Main where
 
 import           Control.DeepSeq                 (NFData, NFData1, force)
 import           Control.Monad                   (return)
-import           Data.ByteString.Lazy            (ByteString)
 import           Data.Function                   (const, ($))
 import           Data.Functor.Rep                (Representable (..))
-import           Data.Semigroup                  ((<>))
-import           Data.String                     (String, fromString)
+import           Data.String                     (String)
 import           System.IO                       (IO)
 import           Test.Tasty.Bench
-import           Test.Tasty.Golden               (goldenVsString)
-import           Text.Show                       (show)
 
 import           ZkFold.Base.Algebra.Basic.Class (zero)
 import           ZkFold.Symbolic.Class           (Arithmetic)
 import           ZkFold.Symbolic.Compiler
 import           ZkFold.Symbolic.Examples        (ExampleOutput (..), examples)
-
-metrics :: String -> ArithmeticCircuit a p i o -> ByteString
-metrics name circuit =
-  fromString name
-  <> "\nNumber of constraints: " <> fromString (show $ acSizeN circuit)
-  <> "\nNumber of variables: " <> fromString (show $ acSizeM circuit)
-  <> "\nNumber of range lookups: " <> fromString (show $ acSizeR circuit)
 
 benchmark ::
   (Arithmetic a, NFData1 o, NFData (Rep i), Representable p, Representable i) =>
@@ -30,13 +19,8 @@ benchmark ::
 benchmark name circuit = bgroup name
   [ bench "compilation" $ nf circuit ()
   , env (return $ force $ circuit ()) $ \c ->
-    let
-        input = tabulate (const zero)
-        path = "stats/" <> name
-     in bgroup "on compilation"
-          [ bench "evaluation" $ nf (witnessGenerator c) input
-          , goldenVsString "# of constraints" path $ return (metrics name c)
-          ]
+    let input = tabulate (const zero)
+     in bench "evaluation" $ nf (witnessGenerator c) input
   ]
 
 main :: IO ()
