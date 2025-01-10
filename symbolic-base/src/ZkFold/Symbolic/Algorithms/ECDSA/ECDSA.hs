d4af28e6e5973f6c3565cf2586bf327ba86d67f3
Eitan Chatav <eitan.chatav@gmail.com>
2025-01-10 09:26:05 -0800

    Update ECDSA.hs

--- symbolic-base/src/ZkFold/Symbolic/Algorithms/ECDSA/ECDSA.hs
+++ symbolic-base/src/ZkFold/Symbolic/Algorithms/ECDSA/ECDSA.hs
@@ -19,27 +19,43 @@ import           ZkFold.Symbolic.Data.Eq
 import           ZkFold.Symbolic.Data.FieldElement       (FieldElement)
 import           ZkFold.Symbolic.Data.UInt               (UInt, eea)
 
-ecdsaVerify :: forall curve n c . (
-      S.Symbolic c
-    , KnownNat n
-    , EllipticCurve curve
-    , BaseField curve ~ UInt 256 'Auto c
-    , Scale (FieldElement c) (Point curve)
-    , Log2 (Order (S.BaseField c) GHC.TypeNats.- 1) ~ 255
-    , SemiEuclidean (UInt 256 'Auto c)
-    , KnownNat (NumberOfRegisters (S.BaseField c) 256 'Auto)
-    , BooleanOf curve ~ Bool c
-    )
-    => Point curve
-    -> ByteString 256 c
-    -> (UInt 256 'Auto c, UInt 256 'Auto c)
-    -> Bool c
+-- ecdsaVerify :: forall curve n c . (
+--       S.Symbolic c
+--     , KnownNat n
+--     , EllipticCurve curve
+--     , BaseField curve ~ UInt 256 'Auto c
+--     , Scale (FieldElement c) (Point curve)
+--     , Log2 (Order (S.BaseField c) GHC.TypeNats.- 1) ~ 255
+--     , SemiEuclidean (UInt 256 'Auto c)
+--     , KnownNat (NumberOfRegisters (S.BaseField c) 256 'Auto)
+--     , BooleanOf curve ~ Bool c
+--     )
+--     => Point curve
+--     -> ByteString 256 c
+--     -> (UInt 256 'Auto c, UInt 256 'Auto c)
+--     -> Bool c
+ecdsaVerify
+  :: forall n c curve baseField scalarField point.
+     ( S.Symbolic c
+     , KnownNat n
+     , baseField ~ UInt 256 'Auto c
+     , scalarField ~ FieldElement c
+     , point ~ Weierstrass curve (Point (Bool c))
+     , SubgroupCurve curve (Bool c) baseField scalarField point
+     , SemiEuclidean (UInt 256 'Auto c)
+     , KnownNat (NumberOfRegisters (S.BaseField c) 256 'Auto)
+     , Log2 (Order (S.BaseField c) GHC.TypeNats.- 1) ~ 255
+     )
+  => point baseField
+  -> ByteString 256 c
+  -> (UInt 256 'Auto c, UInt 256 'Auto c)
+  -> Bool c
 ecdsaVerify publicKey message (r, s) =
-    case c of Point x _ isInf  -> if isInf then false else r == (x `mod` n)
+    case c of Weierstrass (Point x _ isInf) -> if isInf then false else r == (x `mod` n)
     where
         n = fromConstant $ value @n
 
-        g = pointGen
+        g = pointGen @curve @(Bool c) @baseField @scalarField
 
         (sInv, _, _) = eea s n
 
