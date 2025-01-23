1beb5370e9c58464afc6e97b7361dbede3f82869
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2025-01-23 16:42:58 +0100

    Update tests

--- symbolic-base/test/Main.hs
+++ symbolic-base/test/Main.hs
@@ -1,46 +1,53 @@
 module Main where
 
 import           Prelude                   hiding (Bool, Fractional (..), Num (..), drop, length, replicate, take, (==))
-import           Tests.ArithmeticCircuit   (specArithmeticCircuit)
-import           Tests.Arithmetization     (specArithmetization)
-import           Tests.Binary              (specBinary)
-import           Tests.Blake2b             (specBlake2b)
-import           Tests.ByteString          (specByteString)
-import           Tests.Compiler            (specCompiler)
-import           Tests.EllipticCurve       (specEllipticCurve)
-import           Tests.FFA                 (specFFA)
-import           Tests.Field               (specField)
-import           Tests.GroebnerBasis       (specGroebner)
-import           Tests.Group               (specAdditiveGroup)
-import           Tests.Hash                (specHash)
-import           Tests.IVC                 (specIVC)
-import           Tests.List                (specList)
-import           Tests.NonInteractiveProof (specNonInteractiveProof)
-import           Tests.Pairing             (specPairing)
-import           Tests.Permutations        (specPermutations)
-import           Tests.Plonkup             (specPlonkup)
-import           Tests.ReedSolomon         (specReedSolomon)
-import           Tests.RSA                 (specRSA)
-import           Tests.SHA2                (specSHA2, specSHA2Natural)
-import           Tests.UInt                (specUInt)
-import           Tests.Univariate          (specUnivariate)
+
+import           Tests.Base.Algebra.EllipticCurve        (specEllipticCurve)
+import           Tests.Base.Algebra.Field                (specField)
+import           Tests.Base.Algebra.GroebnerBasis        (specGroebner)
+import           Tests.Base.Algebra.Group                (specAdditiveGroup)
+import           Tests.Base.Algebra.Pairing              (specPairing)
+import           Tests.Base.Algebra.Permutations         (specPermutations)
+import           Tests.Base.Algebra.ReedSolomon          (specReedSolomon)
+import           Tests.Base.Algebra.Univariate           (specUnivariate)
+
+import           Tests.Base.Data.Binary                  (specBinary)
+import           Tests.Base.Protocol.IVC                 (specIVC)
+import           Tests.Base.Protocol.NonInteractiveProof (specNonInteractiveProof)
+import           Tests.Base.Protocol.Plonkup             (specPlonkup)
+import           Tests.Symbolic.Algorithm.Blake2b        (specBlake2b)
+import           Tests.Symbolic.Algorithm.RSA            (specRSA)
+import           Tests.Symbolic.Algorithm.SHA2           (specSHA2, specSHA2Natural)
+import           Tests.Symbolic.ArithmeticCircuit        (specArithmeticCircuit)
+import           Tests.Symbolic.Compiler                 (specCompiler)
+import           Tests.Symbolic.Data.ByteString          (specByteString)
+import           Tests.Symbolic.Data.FFA                 (specFFA)
+import           Tests.Symbolic.Data.Hash                (specHash)
+import           Tests.Symbolic.Data.List                (specList)
+import           Tests.Symbolic.Data.UInt                (specUInt)
 
 main :: IO ()
 main = do
-    -- Base
-    specBinary
-
-    -- Algebra
-    specPermutations
+    -- Base.Algebra
     specField
     specAdditiveGroup
-    specPairing
-    specUnivariate
-    specGroebner
     specEllipticCurve
+    specPairing
+    specPermutations
+    specUnivariate
     specReedSolomon
+    specGroebner
+
+    -- Base.Data
+    specBinary
+
+    -- Base.Protocol
+    specPlonkup
+    specNonInteractiveProof
+    specIVC
 
     -- Compiler spec
+    specArithmeticCircuit
     specCompiler
 
     -- Symbolic types and operations
@@ -50,21 +57,10 @@ main = do
     specFFA
     specByteString
 
-    -- Arithmetic circuit
-    specArithmeticCircuit
-
-    -- Arithmetization
-    specArithmetization
-
-    -- Protocols
-    specPlonkup
-    specNonInteractiveProof
-    specIVC
-
-    -- Cryptography
+    -- Symbolic cryptography
+    specBlake2b
+    specRSA
     specSHA2Natural
     specSHA2
-    specRSA
-    specBlake2b
 
     putStrLn "\nAll tests passed!"
---
+++ symbolic-base/test/Tests/Base/Algebra/Pairing.hs
@@ -0,0 +1,89 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE OverloadedLists     #-}
+{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE TypeOperators       #-}
+
+module Tests.Base.Algebra.Pairing (specPairing) where
+
+import           Data.Kind                                   (Type)
+import           Data.Typeable                               (Typeable, typeOf)
+import qualified Data.Vector                                 as V
+import           Prelude                                     hiding (Fractional (..), Num (..), length, (^))
+import           Test.Hspec
+import           Test.QuickCheck
+
+import           ZkFold.Base.Algebra.Basic.Class
+import           ZkFold.Base.Algebra.EllipticCurve.BLS12_381
+import           ZkFold.Base.Algebra.EllipticCurve.BN254
+import           ZkFold.Base.Algebra.EllipticCurve.Class
+import           ZkFold.Base.Algebra.Polynomials.Univariate  (PolyVec, deg, evalPolyVec, polyVecDiv, scalePV, toPolyVec,
+                                                              vec2poly)
+import           ZkFold.Base.Protocol.NonInteractiveProof    (CoreFunction (..), HaskellCore, msm)
+
+propVerificationKZG
+    :: forall c1 c2 f core
+    .  Pairing c1 c2
+    => f ~ ScalarField c1
+    => f ~ ScalarField c2
+    => Field f
+    => Eq f
+    => AdditiveGroup (BaseField c1)
+    => CoreFunction c1 core
+    => f -> PolyVec f 32 -> f -> Bool
+propVerificationKZG x p z =
+    let n  = deg $ vec2poly p
+
+        -- G1
+        gs = V.fromList $ map ((`mul` pointGen) . (x^)) [0 .. n]
+        g0 = V.head gs :: Point c1
+
+        -- G2
+        h0 = pointGen :: Point c2
+        h1 = x `mul` h0
+
+        com = msm @c1 @core
+        -- Proving a polynomial evaluation
+        pz = p `evalPolyVec` z
+        h  = (p - scalePV pz one) `polyVecDiv` toPolyVec [negate z, one]
+        w  = gs `com` h
+        v0 = gs `com` p - (pz `mul` g0) + z `mul` w
+
+        -- Verification
+    in pairing v0 h0 == pairing w h1
+
+specPairing'
+    :: forall (c1 :: Type) (c2 :: Type) f
+    .  Typeable c1
+    => Typeable c2
+    => Typeable (TargetGroup c1 c2)
+    => Pairing c1 c2
+    => f ~ ScalarField c1
+    => Field f
+    => Eq f
+    => Show f
+    => Arbitrary f
+    => Show (BaseField c2)
+    => AdditiveGroup (BaseField c1)
+    => Show (BaseField c1)
+    => BooleanOf c1 ~ Bool
+    => BooleanOf c2 ~ Bool
+    => IO ()
+specPairing' = hspec $ do
+    describe "Elliptic curve pairing specification (SLOW)" $ do
+        describe ("Type: " ++ show (typeOf (pairing @c1 @c2))) $ do
+            describe "Pairing axioms" $ do
+                it "should satisfy bilinearity" $ withMaxSuccess 10 $ do
+                    property $ \a b p q ->
+                        pairing @c1 @c2 (a `mul` p) (b `mul` q)
+                            == pairing p q ^ (a * b)
+                it "should satisfy non-degeneracy" $ withMaxSuccess 10 $ do
+                    property $ \p q ->
+                        (p /= zero && q /= zero) ==> pairing @c1 @c2 p q /= one
+            describe "Pairing verification" $ do
+                it "should verify KZG commitments" $ withMaxSuccess 10 $ do
+                    property $ propVerificationKZG @c1 @c2 @f @HaskellCore
+
+specPairing :: IO ()
+specPairing = do
+    specPairing' @BN254_G1 @BN254_G2
+    specPairing' @BLS12_381_G1 @BLS12_381_G2
