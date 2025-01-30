18fdace1b4c36e6d2dfa1177922a7b7570289b8e
Vladimir <me.morozovvp@gmail.com>
2025-01-30 20:20:43 +1000

    No way, JWT is compiling

--- symbolic-base/src/ZkFold/Symbolic/Data/VarByteString.hs
+++ symbolic-base/src/ZkFold/Symbolic/Data/VarByteString.hs
@@ -5,14 +5,13 @@
 {-# LANGUAGE TypeOperators        #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-{-# OPTIONS_GHC -freduction-depth=0 #-} -- Avoid reduction overflow error caused by NumberOfRegisters
-{-# OPTIONS_GHC -Wno-redundant-constraints #-}
-
 module ZkFold.Symbolic.Data.VarByteString
     ( VarByteString (..)
     , append
     , (@+)
     , shiftL
+    , shiftR
+    , wipeUnassigned
     ) where
 
 import           Control.DeepSeq                   (NFData)
@@ -22,30 +21,22 @@ import           Data.Kind                         (Type)
 import           Data.Proxy                        (Proxy (..))
 import           Data.String                       (IsString (..))
 import           GHC.Generics                      (Generic, Par1 (..))
-import           GHC.TypeLits                      (KnownSymbol (..), Symbol, symbolVal)
-import           Prelude                           (Integer, const, drop, fmap, otherwise, pure, return, take, type (~),
-                                                    ($), (.), (<$>), (<), (<>), (==), (>=))
+import           GHC.TypeLits                      (KnownSymbol (..), symbolVal)
+import           Prelude                           (type (~), ($), (.), (<$>))
 import qualified Prelude                           as Haskell
-import           Test.QuickCheck                   (Arbitrary (..), chooseInteger)
 
 import           ZkFold.Base.Algebra.Basic.Class
-import           ZkFold.Base.Algebra.Basic.Field   (Zp)
 import           ZkFold.Base.Algebra.Basic.Number
-import           ZkFold.Base.Data.HFunctor         (HFunctor (..))
-import           ZkFold.Base.Data.Package          (packWith, unpackWith)
-import           ZkFold.Base.Data.Utils            (zipWithM)
-import qualified ZkFold.Base.Data.Vector           as V
-import           ZkFold.Base.Data.Vector           (Vector (..))
-import           ZkFold.Prelude                    (replicateA, (!!))
+import           ZkFold.Base.Data.Vector           (Vector)
 import           ZkFold.Symbolic.Class
 import           ZkFold.Symbolic.Data.Bool         (Bool (..), BoolType (..))
-import           ZkFold.Symbolic.Data.ByteString   (ByteString (..), Resize (..), ShiftBits (..), isSet)
+import           ZkFold.Symbolic.Data.ByteString   (ByteString (..), ShiftBits (..), isSet)
 import           ZkFold.Symbolic.Data.Class        (SymbolicData)
 import           ZkFold.Symbolic.Data.Combinators
 import           ZkFold.Symbolic.Data.Conditional  (Conditional, bool)
 import           ZkFold.Symbolic.Data.Eq           (Eq)
 import           ZkFold.Symbolic.Data.FieldElement (FieldElement)
-import           ZkFold.Symbolic.Data.Input        (SymbolicInput, isValid)
+import           ZkFold.Symbolic.Data.Input        (SymbolicInput)
 
 -- | A ByteString that has length unknown at compile time but guaranteed to not exceed @maxLen@.
 -- The unassigned buffer space (i.e. bits past @bsLength@) should be set to zero at all times.
@@ -59,26 +50,26 @@ data VarByteString (maxLen :: Natural) (context :: (Type -> Type) -> Type) =
         }
     deriving (Generic)
 
-deriving stock instance (Haskell.Show (c (Vector n)), Haskell.Show (c Par1)) => Haskell.Show (VarByteString n c)
-deriving stock instance (Haskell.Eq (c (Vector n)), Haskell.Eq (c Par1)) => Haskell.Eq (VarByteString n c)
-deriving anyclass instance (NFData (c (Vector n)), NFData (c Par1)) => NFData (VarByteString n c)
-deriving instance (KnownNat n, Symbolic c) => SymbolicData (VarByteString n c)
-deriving instance (KnownNat n, Symbolic c) => SymbolicInput (VarByteString n c)
-deriving instance (Symbolic c, KnownNat n) => Eq (Bool c) (VarByteString n c)
-deriving instance (Symbolic c, KnownNat n) => Conditional (Bool c) (VarByteString n c)
+deriving stock instance (Haskell.Show (ctx (Vector n)), Haskell.Show (ctx Par1)) => Haskell.Show (VarByteString n ctx)
+deriving stock instance (Haskell.Eq (ctx (Vector n)), Haskell.Eq (ctx Par1)) => Haskell.Eq (VarByteString n ctx)
+deriving anyclass instance (NFData (ctx (Vector n)), NFData (ctx Par1)) => NFData (VarByteString n ctx)
+deriving instance (KnownNat n, Symbolic ctx) => SymbolicData (VarByteString n ctx)
+deriving instance (KnownNat n, Symbolic ctx) => SymbolicInput (VarByteString n ctx)
+deriving instance (Symbolic ctx, KnownNat n) => Eq (Bool ctx) (VarByteString n ctx)
+deriving instance (Symbolic ctx, KnownNat n) => Conditional (Bool ctx) (VarByteString n ctx)
 
 instance
-    ( Symbolic c
+    ( Symbolic ctx
     , m * 8 ~ n
     , KnownNat m
-    ) => IsString (VarByteString n c) where
+    ) => IsString (VarByteString n ctx) where
     fromString = fromConstant . fromString @Bytes.ByteString
 
 instance
-    ( Symbolic c
+    ( Symbolic ctx
     , m * 8 ~ n
     , KnownNat m
-    ) => FromConstant Bytes.ByteString (VarByteString n c) where
+    ) => FromConstant Bytes.ByteString (VarByteString n ctx) where
     fromConstant bytes = VarByteString (fromConstant @Natural . (*8) . Haskell.fromIntegral $ Bytes.length bytes) (fromConstant bytes)
 
 instance (Symbolic ctx, KnownNat m, m * 8 ~ n) => FromJSON (VarByteString n ctx) where
@@ -100,17 +91,17 @@ instance
 -- uu1010 `append` u10010 == uuu101010010
 --
 append
-    :: forall m n c
-    .  Symbolic c
+    :: forall m n ctx
+    .  Symbolic ctx
     => KnownNat m
     => KnownNat n
     => KnownNat (m + n)
-    => VarByteString m c
-    -> VarByteString n c
-    -> VarByteString (m + n) c
+    => VarByteString m ctx
+    -> VarByteString n ctx
+    -> VarByteString (m + n) ctx
 append (VarByteString l1 bs1) (VarByteString l2 bs2) = VarByteString (l1 + l2) newBs
     where
-        ex1, ex2 :: ByteString (m + n) c
+        ex1, ex2 :: ByteString (m + n) ctx
         ex1 = resize bs1
         ex2 = resize bs2
 
@@ -118,31 +109,58 @@ append (VarByteString l1 bs1) (VarByteString l2 bs2) = VarByteString (l1 + l2) n
 
 infixr 6 @+
 (@+)
-    :: forall m n c
-    .  Symbolic c
+    :: forall m n ctx
+    .  Symbolic ctx
     => KnownNat m
     => KnownNat n
     => KnownNat (m + n)
-    => VarByteString m c
-    -> VarByteString n c
-    -> VarByteString (m + n) c
+    => VarByteString m ctx
+    -> VarByteString n ctx
+    -> VarByteString (m + n) ctx
 (@+) = append
 
-shiftL
-    :: forall n c
-    .  Symbolic c
+shift
+    :: forall n ctx
+    .  Symbolic ctx
     => KnownNat n
-    => ByteString n c
-    -> FieldElement c
-    -> ByteString n c
-shiftL bs el = Haskell.foldr (\s b -> bool b (b `shiftBitsL` (2^s)) (isSet elBits s)) bs [0 .. nbits]
+    => (ByteString n ctx -> Natural -> ByteString n ctx)
+    -> ByteString n ctx
+    -> FieldElement ctx
+    -> ByteString n ctx
+shift sh bs el = Haskell.foldr (\s b -> bool b (b `sh` (2^s)) (isSet elBits s)) bs [0 .. nbits]
     where
         elBits = ByteString $ binaryExpansion el
 
-        -- No need to perform more shifts than this. 
+        -- No need to perform more shifts than this.
         -- The bytestring will be all zeros beyond this iteration.
         nbits = ilog2 $ value @n
 
         ilog2 :: Natural -> Natural
         ilog2 1 = 0
         ilog2 n = 1 + ilog2 (n `div` 2)
+
+shiftL
+    :: forall n ctx
+    .  Symbolic ctx
+    => KnownNat n
+    => ByteString n ctx
+    -> FieldElement ctx
+    -> ByteString n ctx
+shiftL = shift shiftBitsL
+
+shiftR
+    :: forall n ctx
+    .  Symbolic ctx
+    => KnownNat n
+    => ByteString n ctx
+    -> FieldElement ctx
+    -> ByteString n ctx
+shiftR = shift shiftBitsR
+
+-- | Set all the unassigned bits to zero
+--
+wipeUnassigned :: forall n ctx . (Symbolic ctx, KnownNat n) => VarByteString n ctx -> VarByteString n ctx
+wipeUnassigned VarByteString{..} = VarByteString bsLength ((`shiftR` unassigned) . (`shiftL` unassigned) $ bsBuffer)
+    where
+        unassigned :: FieldElement ctx
+        unassigned = fromConstant (value @n) - bsLength
