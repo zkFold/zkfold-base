fc786920fc0f949c126017704a65f4ce8e6aed0d
vks4git <vks4git@users.noreply.github.com>
2025-01-31 13:22:46 +0000

    stylish-haskell auto-commit

--- symbolic-base/src/ZkFold/Symbolic/Data/Combinators.hs
+++ symbolic-base/src/ZkFold/Symbolic/Data/Combinators.hs
@@ -110,7 +110,7 @@ type family GetRegisterSize (a :: Type) (bits :: Natural) (r :: RegisterSize) ::
 type KnownRegisters c bits r = KnownNat (NumberOfRegisters (BaseField c) bits r)
 
 type family NumberOfRegisters (a :: Type) (bits :: Natural) (r :: RegisterSize ) :: Natural where
-  NumberOfRegisters a bits (Fixed bits) = 1 
+  NumberOfRegisters a bits (Fixed bits) = 1
   NumberOfRegisters a bits (Fixed rs) = If (Mod bits rs >? 0 ) (Div bits rs + 1) (Div bits rs) -- if rs <= maxregsize a, ceil (n / rs)
   NumberOfRegisters a bits Auto       = NumberOfRegisters' a bits (ListRange 1 50) -- TODO: Compilation takes ages if this constant is greater than 10000.
                                                                           -- But it is weird anyway if someone is trying to store a value
@@ -198,7 +198,7 @@ type family FromMaybe (a :: k) (mb :: Haskell.Maybe k) :: k where
     FromMaybe def (Haskell.Just a) = a
 
 type family Length' (s :: Haskell.Maybe (Haskell.Char, Symbol)) :: Natural where
-    Length' 'Haskell.Nothing = 0 
+    Length' 'Haskell.Nothing = 0
     Length' ('Haskell.Just '(c, rest)) = 1 + Length' (UnconsSymbol rest)
 
 ---------------------------------------------------------------
--- symbolic-examples/src/Examples/JWT.hs
+++ symbolic-examples/src/Examples/JWT.hs
@@ -12,7 +12,7 @@ import           ZkFold.Symbolic.Data.VarByteString (VarByteString)
 --exampleJWTSerialisation = secretBits
 
 --exampleJWTSerialisation :: (Symbolic c, NFData (c (Vector 1))) => TokenPayload c -> VarByteString 9456 c
-exampleJWTSerialisation 
+exampleJWTSerialisation
     :: Symbolic c
     => NFData (c (Vector 1))
     => NFData (c (Vector 8))
@@ -20,6 +20,6 @@ exampleJWTSerialisation
     => NFData (c (Vector 864))
     => NFData (c (Vector 9456))
     => NFData (c (Vector 10328))
-    => NFData (c Par1) 
+    => NFData (c Par1)
     => ClientSecret c -> VarByteString 10328 c
-exampleJWTSerialisation = secretBits 
+exampleJWTSerialisation = secretBits
