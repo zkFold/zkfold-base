cb053d27e4ee42263506338dc75cc45045ab6b55
Vladimir <me.morozovvp@gmail.com>
2025-02-01 22:02:08 +1000

    Pedantic build

--- symbolic-examples/src/Examples/JWT.hs
+++ symbolic-examples/src/Examples/JWT.hs
@@ -1,19 +1,12 @@
 module Examples.JWT (exampleJWTSerialisation) where
 
-import           Control.DeepSeq                    (NFData)
-import           GHC.Generics                       (Par1)
-
-import           ZkFold.Base.Data.Vector            (Vector)
 import           ZkFold.Symbolic.Class
-import           ZkFold.Symbolic.Data.ByteString    (ByteString, shiftBitsL)
-import           ZkFold.Symbolic.Data.FieldElement
 import           ZkFold.Symbolic.Data.JWT           (ClientSecret, SecretBits, secretBits)
-import           ZkFold.Symbolic.Data.VarByteString (VarByteString, append, shiftL)
+import           ZkFold.Symbolic.Data.VarByteString (VarByteString)
 
 exampleJWTSerialisation
     :: Symbolic c
     => SecretBits c
-    => NFData (c Par1)
     => ClientSecret c
     -> VarByteString 10328 c
 exampleJWTSerialisation = secretBits
