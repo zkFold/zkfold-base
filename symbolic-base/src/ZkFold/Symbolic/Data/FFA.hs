e8367690935e44402b62f00b562e8b5321a52b9b
TurtlePU <sokolov.p64@gmail.com>
2025-02-05 21:53:05 +0300

    + more FFA stubs

--- symbolic-base/src/ZkFold/Symbolic/Data/FFA.hs
+++ symbolic-base/src/ZkFold/Symbolic/Data/FFA.hs
@@ -2,29 +2,34 @@
 {-# LANGUAGE NoStarIsType         #-}
 {-# LANGUAGE TypeOperators        #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE BlockArguments #-}
 
 module ZkFold.Symbolic.Data.FFA (FFA (..)) where
 
 import           Control.DeepSeq                   (NFData)
+import           Data.Bool                         (otherwise)
+import           Data.Ord                          ((<))
 import           Data.Type.Equality                (type (~))
-import           GHC.Generics                      (Generic)
+import           GHC.Generics                      (Generic, type (:*:) (..), U1 (..), Par1 (..))
 import           Numeric.Natural                   (Natural)
-import           Prelude                           (Integer)
+import           Prelude                           (Integer, fromIntegral)
 import           Text.Show                         (Show)
 
 import           ZkFold.Base.Algebra.Basic.Class
 import           ZkFold.Base.Algebra.Basic.Field   (Zp)
-import           ZkFold.Base.Algebra.Basic.Number  (KnownNat, Prime, type (*))
+import           ZkFold.Base.Algebra.Basic.Number  (Bezout1, KnownInt (..), KnownNat, Prime, type (*), type (^), value)
 import           ZkFold.Symbolic.Class             (Arithmetic, Symbolic (..))
-import           ZkFold.Symbolic.Data.Bool         (Bool)
+import           ZkFold.Symbolic.Data.Bool         (Bool, BoolType (..))
 import           ZkFold.Symbolic.Data.Class        (SymbolicData (..))
 import           ZkFold.Symbolic.Data.Combinators  (Ceil, KnownRegisterSize, KnownRegisters)
-import           ZkFold.Symbolic.Data.Conditional  (Conditional)
-import           ZkFold.Symbolic.Data.Eq           (Eq)
+import           ZkFold.Symbolic.Data.Conditional  (Conditional (..))
+import           ZkFold.Symbolic.Data.Eq           (Eq (..))
 import           ZkFold.Symbolic.Data.FieldElement (FieldElement)
-import           ZkFold.Symbolic.Data.Input        (SymbolicInput)
+import           ZkFold.Symbolic.Data.Input        (SymbolicInput (..))
 import           ZkFold.Symbolic.Data.UInt         (UInt)
 import           ZkFold.Symbolic.Interpreter       (Interpreter (..))
+import Data.Proxy (Proxy(..))
+import Data.Function (($), const)
 
 type family FFAUIntSize (p :: Natural) (q :: Natural) :: Natural where
   FFAUIntSize p p = 0
@@ -38,13 +43,18 @@ data FFA p r c = FFA
   }
   deriving (Generic)
 
+type FFABezout p a = Bezout1 p (2 ^ FFAUIntSize p (Order a))
+
 type KnownFFA p r c =
   ( KnownNat (FFAUIntSize p (Order (BaseField c)))
+  , KnownInt (FFABezout p (BaseField c))
+  , KnownNat p
   , KnownRegisterSize r
   , KnownRegisters c (FFAUIntSize p (Order (BaseField c))) r)
 
 instance (Symbolic c, KnownFFA p r c) => SymbolicData (FFA p r c)
-instance (Symbolic c, KnownFFA p r c) => SymbolicInput (FFA p r c)
+instance (Symbolic c, KnownFFA p r c) => SymbolicInput (FFA p r c) where
+  isValid (FFA nx ux) = isValid (nx, ux) -- TODO: add range check
 instance (NFData (FieldElement c), NFData (UIntFFA p r c)) => NFData (FFA p r c)
 instance (Symbolic c, KnownFFA p r c, b ~ Bool c) => Conditional b (FFA p r c)
 instance (Symbolic c, KnownFFA p r c) => Eq (FFA p r c)
@@ -55,49 +65,70 @@ instance (Arithmetic a, KnownFFA p r (Interpreter a)) =>
     ToConstant (FFA p r (Interpreter a)) where
   type Const (FFA p r (Interpreter a)) = Zp p
   toConstant (FFA nx ux) =
-    let n = toConstant (toConstant nx)
-        u = toConstant ux
+    let n = fromConstant (toConstant (toConstant nx))
+        u = fromConstant (toConstant ux) :: Integer
         -- x = k|a| + n = l*2^s + u
         -- k|a| - l*2^s = u - n
-     in fromConstant _
+        k = (u - n) * intValue @(FFABezout p a)
+     in fromConstant (k * fromConstant (order @a) + n)
 
-instance (FromConstant a (Zp p), Symbolic c) => FromConstant a (FFA p r c) where
-  fromConstant c = _
+instance (Symbolic c, KnownFFA p r c, FromConstant a (Zp p)) =>
+    FromConstant a (FFA p r c) where
+  fromConstant c =
+    let c' = toConstant (fromConstant c :: Zp p)
+     in FFA (fromConstant c') (fromConstant c')
 
 instance {-# OVERLAPPING #-} FromConstant (FFA p r c) (FFA p r c)
 
-instance {-# OVERLAPPING #-} Symbolic c => Scale (FFA p r c) (FFA p r c)
+instance {-# OVERLAPPING #-}
+  (Symbolic c, KnownFFA p r c) => Scale (FFA p r c) (FFA p r c)
 
-instance Symbolic c => MultiplicativeSemigroup (FFA p r c) where
-  FFA nx ux * FFA ny uy = _
+instance (Symbolic c, KnownFFA p r c) => MultiplicativeSemigroup (FFA p r c) where
+  FFA nx ux * FFA ny uy = FFA nr ur
+    where
+      nd, ns :: FieldElement c
+      ud, us :: UIntFFA p r c
+      (nd, ud, ns, us) = restore $ const
+        ( fromCircuitF (arithmetize (nx, ux, ny, uy) Proxy)
+            \((Par1 ni :*: ui) :*: (Par1 nj :*: uj)) -> do
+              _
+        , (U1 :*: U1) :*: (U1 :*: U1))
 
-instance Symbolic c => Exponent (FFA p r c) Natural where
-  FFA nx ux ^ a = _
+instance (Symbolic c, KnownFFA p r c) => Exponent (FFA p r c) Natural where
+  x ^ a = x `natPow` (a `mod` (value @p -! 1))
 
-instance (Symbolic c, KnownNat p) => MultiplicativeMonoid (FFA p r c) where
+instance (Symbolic c, KnownFFA p r c) => MultiplicativeMonoid (FFA p r c) where
   one = fromConstant (one :: Zp p)
 
-instance Symbolic c => AdditiveSemigroup (FFA p r c) where
-  FFA nx ux + FFA ny uy = _
+instance (Symbolic c, KnownFFA p r c) => AdditiveSemigroup (FFA p r c) where
+  FFA nx ux + FFA ny uy = FFA nr ur
 
-instance (Scale a (Zp p), KnownNat p, Symbolic c) => Scale a (FFA p r c) where
+instance (Symbolic c, KnownFFA p r c, Scale a (Zp p)) => Scale a (FFA p r c) where
   scale k x = fromConstant (scale k one :: Zp p) * x
 
-instance (Symbolic c, KnownNat p) => AdditiveMonoid (FFA p r c) where
+instance (Symbolic c, KnownFFA p r c) => AdditiveMonoid (FFA p r c) where
   zero = fromConstant (zero :: Zp p)
 
-instance (Symbolic c, KnownNat p) => AdditiveGroup (FFA p r c) where
-  negate (FFA nx ux) = _
+instance (Symbolic c, KnownFFA p r c) => AdditiveGroup (FFA p r c) where
+  negate (FFA nx ux) = bool
+    (FFA (fromConstant (value @p) - nx) (fromConstant (value @p) - ux))
+    (FFA nx ux)
+    (nx == zero && ux == zero)
 
-instance (Symbolic c, KnownNat p) => Semiring (FFA p r c)
+instance (Symbolic c, KnownFFA p r c) => Semiring (FFA p r c)
 
-instance (Symbolic c, KnownNat p) => Ring (FFA p r c)
+instance (Symbolic c, KnownFFA p r c) => Ring (FFA p r c)
 
-instance (Symbolic c, Prime p) => Exponent (FFA p r c) Integer where
-  x ^ a = _
+instance (Symbolic c, KnownFFA p r c, Prime p) => Exponent (FFA p r c) Integer where
+  x ^ a
+    | neg < pos = finv x ^ neg
+    | otherwise = x ^ pos
+    where
+      pos = fromIntegral (a `mod` fromIntegral (value @p -! 1))
+      neg = value @p -! (pos + 1)
 
-instance (Symbolic c, Prime p) => Field (FFA p r c) where
-  finv (FFA nx ux) = _
+instance (Symbolic c, KnownFFA p r c, Prime p) => Field (FFA p r c) where
+  finv = _
 
 instance Finite (Zp p) => Finite (FFA p r b) where
   type Order (FFA p r b) = p
