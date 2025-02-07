406e267cb8f099c8888d184afe957f922252a556
Vladimir <me.morozovvp@gmail.com>
2025-02-07 17:07:51 +1000

    Optimised JWT serialisation; fixed bug in Blake2b

--- symbolic-base/src/ZkFold/Symbolic/Data/JWT.hs
+++ symbolic-base/src/ZkFold/Symbolic/Data/JWT.hs
@@ -15,11 +15,6 @@ module ZkFold.Symbolic.Data.JWT
     , secretBits
     , toAsciiBits
     , verifySignature
-
-
--------------------------------------------------
-    , base64ToAscii
-    , padBytestring6
     ) where
 
 import           Control.DeepSeq                    (NFData, force)
