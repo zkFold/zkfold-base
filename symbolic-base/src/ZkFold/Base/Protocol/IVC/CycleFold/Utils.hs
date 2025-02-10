c5c3ae701a8392476cebd5ed973cf338f853bc66
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2025-02-10 08:20:18 +0100

    Add comments

--- symbolic-base/src/ZkFold/Base/Protocol/IVC/CycleFold/Utils.hs
+++ symbolic-base/src/ZkFold/Base/Protocol/IVC/CycleFold/Utils.hs
@@ -2,10 +2,10 @@ module ZkFold.Base.Protocol.IVC.CycleFold.Utils where
 
 import           GHC.Generics (Par1 (..))
 
-type ForeignField = Par1
+type ForeignField = Par1 -- functor for transforming into "native context" (see NativeContext.hs)
 
-type PrimaryField = Par1
-type PrimaryGroup = Par1
+type PrimaryField = Par1 -- Fq
+type PrimaryGroup = Par1 -- Point with Fp coordinates
 
-type SecondaryField = Par1
-type SecondaryGroup = Par1
+type SecondaryField = Par1 -- Fp
+type SecondaryGroup = Par1 -- Point with Fq coordinates
