34fd5ecdbe8a46495b7aaa10c478d230f4243650
Eitan Chatav <eitan.chatav@gmail.com>
2025-02-11 09:01:21 -0800

    Update Class.hs

--- symbolic-base/src/ZkFold/Symbolic/Class.hs
+++ symbolic-base/src/ZkFold/Symbolic/Class.hs
@@ -93,17 +93,6 @@ fromCircuit3F ::
 -- | Runs the ternary @'CircuitFun'@ in a generic context.
 fromCircuit3F x y z m = fromCircuit2F (hpair x y) z (uncurryP m)
 
-symbolic4F ::
-  (Symbolic c, BaseField c ~ a) => c f -> c g -> c h -> c k ->
-  (f a -> g a -> h a -> k a -> j a) -> CircuitFun '[f, g, h, k] j c -> c j
--- | Runs the ternary function from @f@, @g@ and @h@ into @k@ in a context @c@.
-symbolic4F x y z w f m = symbolic3F (hpair x y) z w (uncurryP f) (uncurryP m)
-
-fromCircuit4F ::
-  Symbolic c => c f -> c g -> c h -> c i -> CircuitFun '[f, g, h, i] k c -> c k
--- | Runs the ternary @'CircuitFun'@ in a generic context.
-fromCircuit4F x y z t m = fromCircuit3F (hpair x y) z t (uncurryP m)
-
 symbolicVF ::
   (Symbolic c, BaseField c ~ a, WitnessField c ~ w, Foldable f, Functor f) =>
   f (c g) -> (f (g a) -> h a) ->
